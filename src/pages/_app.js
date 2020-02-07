f5cbf2b9dfe67a3c37563a5d5fd28b93c5d8f69f
Justin Ramers <justin@digitalchamps.com>
2020-02-07 15:32:10 -0800

    Integrate redux into _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import App, { Container } from 'next/app'
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import App, { Container } from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import { makeStore } from '../modules';
 
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
@@ -7,11 +11,23 @@ config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatica
 
 import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
-
 library.add(faUser, faEnvelope, faLock, faTwitter, faFacebook)
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+
+const MyApp = ({ Component, pageProps, store }) => {
+    return (
+      <Provider store={store}>
+          <Component {...pageProps} />
+      </Provider>
+    );
+};
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+    // we can dispatch from here too
+    ctx.store.dispatch({ type: 'FOO', payload: 'foo' });
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+    return { pageProps };
+};
+
+export default withRedux(makeStore)(MyApp);
