c76b70417ffc97edf0c4361bdde1daa91fa9632d
Justin Ramers <justin@digitalchamps.com>
2020-02-07 15:31:32 -0800

    Setup auth - index, operations, types

---
+++ src/modules/auth/operations.js
@@ -0,0 +1,13 @@
+import {
+  setBasicUser, login, logout,
+  refreshToken, loginViaToken, fetchCurrentUserIfNeeded,
+} from './actions';
+
+export {
+  setBasicUser,
+  login,
+  logout,
+  refreshToken,
+  loginViaToken,
+  fetchCurrentUserIfNeeded,
+};
