fb6cea87191e9e401909423662ae5d97b0ce0887
Justin Ramers <justin@digitalchamps.com>
2020-02-08 08:30:20 -0800

    Only log redux in dev

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,10 +4,18 @@ import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const logger = createLogger({
-  collapsed: true,
-});
+// Only log in dev
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({
+    collapsed: true,
+  });
 
-export const makeStore = (initialState, options) => {
-    return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk, logger)));
-};
+  export const makeStore = (initialState, options) => {
+      return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk, logger)));
+  };
+} else {
+  export const makeStore = (initialState, options) => {
+      return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+  };
+
+}
