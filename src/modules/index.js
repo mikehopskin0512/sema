e41b6cb79418d9a6a40e2103325f0753488fd8db
Justin Ramers <justin@digitalchamps.com>
2020-02-07 21:00:53 -0800

    Add redux-logger

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
+const logger = createLogger({
+  collapsed: true,
+});
+
 export const makeStore = (initialState, options) => {
-    return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+    return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk, logger)));
 };
