91d16f62bef823a8bf53c9b8e0fef1d7cd4ecb0e
Justin Ramers <justin@digitalchamps.com>
2020-02-08 08:44:58 -0800

    Fix conditional export

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,18 +4,19 @@ import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
+export let makeStore;
+
 // Only log in dev
 if (process.env.NODE_ENV === 'development') {
   const logger = createLogger({
     collapsed: true,
   });
 
-  export const makeStore = (initialState, options) => {
+  makeStore = (initialState, options) => {
       return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk, logger)));
   };
 } else {
-  export const makeStore = (initialState, options) => {
+  makeStore = (initialState, options) => {
       return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
   };
-
 }
