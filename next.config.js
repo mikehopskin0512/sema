4930605d4fb83c3f32ec2890c7d308e4390f6a95
Justin Ramers <jramers@semasoftware.com>
2020-02-10 23:47:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
+const dotEnvResult = require('dotenv').config();
+const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
 
-module.exports = withCSS(withSass())
+const parsedVariables = dotEnvResult.parsed || {};
+const dotEnvVariables = {};
+for (const key of Object.keys(parsedVariables)) {
+  dotEnvVariables[key] = process.env[key];
+}
+module.exports = {
+  env: {
+    ...dotEnvVariables,
+  },
+  ...withCSS(withSass()),
+};
