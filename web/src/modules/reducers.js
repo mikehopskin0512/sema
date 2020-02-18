17c7ed0affad3b6c1698c20590eaf988dadc5305
Justin Ramers <justin@digitalchamps.com>
2020-02-18 16:01:24 -0500

    Clear state tree on logout

--- web/src/modules/reducers.js
+++ web/src/modules/reducers.js
@@ -2,8 +2,19 @@ import { combineReducers } from 'redux';
 
 import auth from './auth/reducers';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   auth,
 });
+
+const rootReducer = (state, action) => {
+  let newState = state;
+
+  // If logout, clear entire state tree
+  if (action.type === 'AUTH_LOGOUT_SUCCESS') {
+    newState = undefined;
+  }
+
+  return appReducer(newState, action);
+};
 
 export default rootReducer;
