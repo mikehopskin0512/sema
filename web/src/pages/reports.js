60deb5866da728046ed21a384cedf5b93e764f42
Justin Ramers <justin@digitalchamps.com>
2020-02-22 17:49:06 -0500

    Update reports page with components

--- web/src/pages/reports.js
+++ web/src/pages/reports.js
@@ -1,5 +1,20 @@
 import withLayout from '../components/layout';
+import ReportsHeader from './reports/reportsHeader';
 
-const Reports = () => <p>This is the reports page.</p>;
+const Reports = () => {
+  return (
+    <div>
+      <ReportsHeader />
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Report Title</h1>
+          <h2 className="subtitle">
+            iFrame of report goes here
+          </h2>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default withLayout(Reports);
