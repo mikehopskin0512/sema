8ba17cbc1dbc871082a8eb1403a2272ba0cb27df
Chris Smith <pangeaware@gmail.com>
2020-02-27 17:23:00 -0800

    refactored auth state

--- web/src/state/features/auth/actions.js
+++ web/src/state/features/auth/actions.js
@@ -1,166 +1,49 @@
+import Router from 'next/router';
 import * as types from './types';
+import {auth} from './api';
+import {removeCookie, setCookie} from '../../utils/cookie';
+const authCookie = process.env.AUTH_JWT;
 
-const loginRequest = () => ({
-  type: types.AUTH_LOGIN_REQUEST,
+const authenticateRequest = () => ({
+  type: types.AUTHENTICATE_REQUEST,
 });
 
-const loginSuccess = (user) => ({
-  type: types.AUTH_LOGIN_SUCCESS,
+const authenticateSuccess = (data) => ({
+  type: types.AUTHENTICATE,
   token: data.token,
-  user,
 });
 
-const loginError = (errors) => ({
-  type: types.AUTH_LOGIN_FAILURE,
+const authenticateError = (errors) => ({
+  type: types.AUTHENTICATE_FAILURE,
   errors,
 });
 
-const logoutSuccess = () => ({
-  type: types.AUTH_LOGOUT_SUCCESS,
+export const authenticate  = (username, password) => async (dispatch) => {
+  dispatch(authenticateRequest());
+  const res = await auth({username,password});
+  if (res.error) {
+    dispatch(authenticateError(res.error));
+  }
+  if (res.data && res.data.token) {
+    setCookie(authCookie, res.data.token);
+    dispatch(authenticateSuccess(res.data));
+    dispatch(hydrateUser(res.data));
+    Router.push('/reports');
+  }
+}
+
+export const hydrateUser = (data) => ({
+  type: types.HYDRATE_USER,
+  user: data,
 });
 
-const loginTokenRequest = () => ({
-  type: types.LOGIN_TOKEN_REQUEST,
-});
-
-const loginTokenSuccess = (token) => ({
-  type: types.LOGIN_TOKEN_SUCCESS,
+export const reauthenticate = (token) => ({
+  type: types.AUTHENTICATE,
   token,
 });
 
-const refreshTokenRequest = () => ({
-  type: types.REFRESH_TOKEN_REQUEST,
-});
-
-const refreshTokenSuccess = (data) => {
-  const now = new Date();
-  const expires = now.setSeconds(now.getSeconds() + data.expires_in);
-
-  return {
-    type: types.REFRESH_TOKEN_SUCCESS,
-    token: data.access_token,
-    expires,
-  }
-}
-
-const refreshTokenError = (error) => ({
-  type: types.REFRESH_TOKEN_ERROR,
-  errors: (error) && error.data,
-});
-
-const requestCurrentUser = () => ({
-  type: types.REQUEST_CURRENT_USER,
-});
-
-const receiveCurrentUser = (data) => ({
-  type: types.RECEIVE_CURRENT_USER,
-  data: data.user,
-});
-
-const fetchCurrentUserError = (error) => ({
-  type: types.REQUEST_CURRENT_USER_ERROR,
-  errors: error.data,
-});
-
-export const setBasicUser = (user) => ({
-  type: types.SET_BASIC_USER,
-  user,
-});
-
-export const logout = () => (dispatch) => {
-  removeCookie('sema_token');
-  dispatch(logoutSuccess());
-  Router.push('/login');
-}
-
-const fetchCurrentUser = (accessToken) => {
-  return function (dispatch) {
-    dispatch(requestCurrentUser());
-    api.get('/proxy/v1/endpoint', {}, accessToken)
-      .then((res) => {
-        dispatch(receiveCurrentUser(res));
-      })
-      .catch((error) => {
-        dispatch(fetchCurrentUserError(error.response));
-        dispatch(logout());
-      });
-  }
-}
-
-const shouldFetchCurrentUser = (state) => {
-  const { user } = state.auth;
-  if (Object.keys(user).length === 0) {
-    return true;
-  }
-
-  return false;
-}
-
-export const fetchCurrentUserIfNeeded = (accessToken) => {
-  return function (dispatch, getState) {
-    if (shouldFetchCurrentUser(getState())) {
-      return dispatch(fetchCurrentUser(accessToken));
-    }
-
-    return false;
-  };
-}
-
-export const login = (email, password) => {
-  return async (dispatch) => {
-    try {
-      dispatch(loginRequest());
-      const res = await api.post('/login', {
-        username: email,
-        password,
-      });
-
-      console.log(res);
-      if (res.success) {
-        setCookie('sema_token', res.token);
-        dispatch(loginSuccess(res));
-        Router.push('/reports');
-      } else {
-        console.log(res);
-        dispatch(loginError(res.error));
-      }
-    } catch (error) {
-      console.log(error);
-      dispatch(loginError(error.response));
-    }
-  }
-}
-
-export const loginViaToken = (accessToken) => {
-  return function (dispatch) {
-    dispatch(loginTokenRequest());
-    dispatch(loginTokenSuccess(accessToken));
-    dispatch(fetchCurrentUser(accessToken));
-  };
-}
-
-export function refreshToken(refreshToken) {
-  return function (dispatch) {
-    dispatch(refreshTokenRequest());
-    api.post('/proxy/oauth/token', {
-      refresh_token: refreshToken,
-      grant_type: 'refresh_token',
-    })
-      .then((data) => {
-        dispatch(setCookie(data));
-        dispatch(refreshTokenSuccess(data));
-        // Moved to checkToken.js
-        // dispatch(fetchCurrentUser(data.access_token));
-      })
-      .catch((error) => {
-        let err = {};
-        if ('response' in error) {
-          err = error.response;
-        } else if ('error_description' in error) {
-          err.error_description = error;
-        }
-        dispatch(refreshTokenError(err));
-        dispatch(logout());
-      });
-  };
+export const deauthenticate = () => (dispatch) => {
+  removeCookie(authCookie);
+  Router.push('/');
+  dispatch({type: types.DEAUTHENTICATE});
 }
--- web/src/state/features/auth/reducers.js
+++ web/src/state/features/auth/reducers.js
@@ -9,55 +9,32 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.AUTH_LOGIN_REQUEST:
+    case types.AUTHENTICATE_REQUEST:
       return {
         ...state,
         isFetching: true,
       }
-    case types.AUTH_LOGIN_SUCCESS:
+    case types.AUTHENTICATE:
       return {
         ...state,
         isFetching: false,
         isAuthenticated: true,
         token: action.token,
+        error: {},
+      }
+    case types.AUTHENTICATE_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        isAuthenticated: false,
+        error: action.errors,
+      }
+    case types.HYDRATE_USER:
+      return {
+        ...state,
         user: action.user,
-        error: {},
       }
-    case types.LOGIN_TOKEN_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        token: action.token,
-        error: {},
-      }
-    case types.AUTH_LOGIN_FAILURE:
-      return {
-        ...state,
-        isFetching: false,
-        isAuthenticated: false,
-        error: action.errors,
-      }
-    case types.REFRESH_TOKEN_REQUEST:
-      return {
-        ...state,
-        isFetching: true,
-      }
-    case types.REFRESH_TOKEN_SUCCESS:
-      return {
-        ...state,
-        isFetching: false,
-        token: action.token,
-        error: {},
-      }
-    case types.REFRESH_TOKEN_ERROR:
-      return {
-        ...state,
-        isFetching: false,
-        token: null,
-        user: {},
-        error: action.errors,
-      }
-    case types.AUTH_LOGOUT_SUCCESS:
+    case types.DEAUTHENTICATE:
       return {
         ...state,
         isAuthenticated: false,
@@ -65,26 +42,6 @@ const reducer = (state = initialState, action) => {
         user: {},
         error: {},
       }
-    case types.REQUEST_CURRENT_USER:
-      return {
-        ...state,
-        isFetching: true,
-      }
-    case types.RECEIVE_CURRENT_USER:
-      return {
-        ...state,
-        isFetching: false,
-        isAuthenticated: true,
-        user: action.data,
-        error: {},
-      }
-    case types.REQUEST_CURRENT_USER_ERROR:
-      return {
-        ...state,
-        isFetching: false,
-        user: {},
-        error: action.errors,
-      };
     default:
       return state;
   }
