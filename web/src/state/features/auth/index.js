fb1a9f1958fb098042c445cd56af2e048a1bfef4
Chris Smith <pangeaware@gmail.com>
2020-02-28 09:36:27 -0800

    removed commented code

--- web/src/state/features/auth/index.js
+++ web/src/state/features/auth/index.js
@@ -1,9 +1,5 @@
 import reducer from './reducers';
 
-// import * as authActions from './actions';
 export { default as authOperations } from "./operations";
-// import * as authSelectors from './selectors';
-
-//export { authOperations, authSelectors, authActions };
 
 export default reducer;
--- web/src/state/store.js
+++ web/src/state/store.js
@@ -1,7 +1,6 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-// import reducer from './reducers';
 import * as reducers from './features';
 
 const middleware = [thunk];
