7956b6433fd96b042f8f0ae8395d3d5c65eedcfe
Chris Smith <pangeaware@gmail.com>
2020-02-28 00:18:10 -0800

    renamed authInspector -> sentry

--- web/src/pages/_app.js
+++ web/src/pages/_app.js
@@ -5,7 +5,7 @@ import { config, library } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
 import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { initialize } from '../state/utils/authInspector';
+import { initialize } from '../state/utils/sentry';
 import { initStore } from '../state/store';
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
