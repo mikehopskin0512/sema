dbd1b9c04a253bf655c2079c08f86315a54dbb90
Justin Ramers <justin@digitalchamps.com>
2020-03-03 10:31:05 -0800

    Add support for token to get and getAll API requests

--- web/src/state/utils/api.js
+++ web/src/state/utils/api.js
@@ -4,9 +4,16 @@ const api = axios.create({
   baseURL: process !== 'undefined' ? process.env.BASE_URL : null,
 });
 
-export const get = (endpoint, { id }) => api.get(`${endpoint}/${id}`);
+export const get = (endpoint, { id }, token) => api.get(`${endpoint}/${id}`, {
+  headers: {
+    Authorization: `Bearer ${token || ''}`,
+  },
+});
 
-export const getAll = (endpoint, { params } = {}) => api.get(endpoint, { params });
+export const getAll = (endpoint, { params } = {}, token) => api.get(endpoint, { params,
+  headers: {
+    Authorization: `Bearer ${token || ''}`,
+  } });
 
 export const create = (endpoint, item) => api.post(endpoint, item);
 
