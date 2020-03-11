ac03f309891b3ac5a0b4800b7bd8cac9df3c907c
Justin Ramers <justin@digitalchamps.com>
2020-03-10 22:48:32 -0700

    Pass reportId and urlParams to getReportUrl

--- web/src/state/features/reports/api.js
+++ web/src/state/features/reports/api.js
@@ -1,4 +1,4 @@
 import { getAll } from '../../utils/api';
 
-export const getReportUrl = (token) => getAll('/v1/embedded_bi', {}, token);
+export const getReportUrl = (params, token) => getAll('/v1/embedded_bi', { params }, token);
 export const getPdfUrl = (token) => getAll('/v1/embedded_bi_pdf', {}, token);
