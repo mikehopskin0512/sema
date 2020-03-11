69696437f621e8b473676caf5c95d47cbd4c6342
Justin Ramers <justin@digitalchamps.com>
2020-03-10 22:48:51 -0700

    Add users state to index

--- web/src/state/features/index.js
+++ web/src/state/features/index.js
@@ -1,2 +1,3 @@
 export { default as authState } from './auth';
-export { default as reportState } from './reports';
+export { default as reportsState } from './reports';
+export { default as usersState } from './users';
