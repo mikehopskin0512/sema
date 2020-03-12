063a7b09780ce5a7182af0ffc61d5f8d3577ba62
Justin Ramers <jramers@semasoftware.com>
2020-03-12 11:36:04 -0700

    Add repositoriesState

--- web/src/state/features/index.js
+++ web/src/state/features/index.js
@@ -1,3 +1,4 @@
 export { default as authState } from './auth';
 export { default as reportsState } from './reports';
+export { default as repositoriesState } from './repositories';
 export { default as usersState } from './users';
