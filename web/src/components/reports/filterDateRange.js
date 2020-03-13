538abcb52ef50432b1e0064f5205f0ddaeb2505c
Justin Ramers <jramers@semasoftware.com>
2020-03-13 14:15:08 -0700

    Refactor date filter to use start/end date pickers

--- web/src/components/reports/filterDateRange.js
+++ web/src/components/reports/filterDateRange.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { format, subDays } from 'date-fns';
-import ReactSelect from 'react-select';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './style.scss';
 
-const dateRanges = [
-  { label: 'Last 7 days', value: 7 },
-  { label: 'Last 14 days', value: 14 },
-  { label: 'Last 28 days', value: 28 },
-  { label: 'Last 60 days', value: 60 },
-  { label: 'Last 90 days', value: 90 },
-  { label: 'Last 180 days', value: 180 },
-  { label: 'Last 365 days', value: 365 },
-  { label: 'All Time', value: 1000 },
-];
+const today = new Date();
+const weekAgo = subDays(today, 7);
 
 const FilterDateRange = (props) => {
+  const [startDate, setStartDate] = useState(weekAgo);
+  const [endDate, setEndDate] = useState(today);
+
   const { updateFilters } = props;
 
-  const buildParams = (option) => {
+  // Update report url when filters change
+  useEffect(() => {
     const paramType = 'filter_dateRange';
-    const { value: daysPast = 7 } = option;
-    const today = format(new Date(), 'yyyy-MM-dd');
-    const startDate = format(subDays(new Date(), daysPast), 'yyyy-MM-dd');
-    const paramList = `param_z_date_end=${today}&param_z_date_start=${startDate}`;
+    const paramList = `param_z_date_end=${format(endDate, 'yyyy-MM-dd')}&param_z_date_start=${format(startDate, 'yyyy-MM-dd')}`;
 
     updateFilters(paramType, paramList);
-  };
+  }, [startDate, endDate]);
 
   return (
-    <ReactSelect
-      hideSelectedOptions
-      options={dateRanges}
-      placeholder="Date range"
-      onChange={(option) => buildParams(option)} />
+    <div>
+      <DatePicker
+        className="input-datepicker"
+        placeholderText="Start date"
+        dateFormat="yyyy-MM-dd"
+        selected={startDate}
+        onChange={(date) => setStartDate(date)} />-
+      <DatePicker
+        className="input-datepicker"
+        placeholderText="End date"
+        dateFormat="yyyy-MM-dd"
+        selected={endDate}
+        onChange={(date) => setEndDate(date)} />
+    </div>
   );
 };
 
