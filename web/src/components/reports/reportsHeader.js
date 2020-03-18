f6694eccc6af7d6c3b1fb225eb05c745fc7a6d77
Justin Ramers <jramers@semasoftware.com>
2020-03-18 08:16:40 -0700

    Move data fetching to reportHeader and pass selectData and param names as props. Use unified function to fetch.

--- web/src/components/reports/reportsHeader.js
+++ web/src/components/reports/reportsHeader.js
@@ -1,16 +1,57 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import FilterDateRange from './filterDateRange';
-import FilterDevelopers from './filterDevelopers';
-import FilterFileTypes from './FilterFileTypes';
-import FilterRepositories from './filterRepositories';
+import DateRangeFilter from './dateRangeFilter';
+import MultiSelectFilter from './multiSelectFilter';
+import _ from 'lodash';
+
+import { organizationsOperations } from '../../state/features/organizations';
+
+const { fetchFilterLists } = organizationsOperations;
 
 const ReportsHeader = (props) => {
   // Create REFs for menus
   const filterMenu = useRef(null);
+  const dispatch = useDispatch();
 
+  // Import state vars
+  const { auth, organizations } = useSelector(
+    (state) => ({
+      auth: state.authState,
+      organizations: state.organizationsState,
+    }),
+  );
+
+  const { user: { organization_id: orgId } } = auth;
   const { updateFilters } = props;
+
+  useEffect(() => {
+    dispatch(fetchFilterLists(orgId, auth.token));
+  }, [dispatch, orgId, auth.token]);
+
+  const { repositories, developers, fileTypes } = organizations;
+
+  const repositoriesList = _.map(repositories, (repository) => (
+    {
+      value: repository.id,
+      label: repository.name,
+    }
+  ));
+
+  const developersList = _.map(developers, (user) => (
+    {
+      value: user.id,
+      label: `${user.first_name} ${user.last_name}`,
+    }
+  ));
+
+  const fileTypesList = _.map(fileTypes, (fileType) => (
+    {
+      value: fileType.id,
+      label: fileType.typename,
+    }
+  ));
 
   const toggleFilterMenu = () => {
     if (filterMenu.current) {
@@ -58,16 +99,31 @@ const ReportsHeader = (props) => {
         <div className="column is-10 is-offset-1">
           <div className="columns">
             <div className="column">
-              <FilterRepositories updateFilters={updateFilters} />
+              <MultiSelectFilter
+                updateFilters={updateFilters}
+                selectData={repositoriesList}
+                paramName="param_z_repositories"
+                placeholder="Repositories" />
             </div>
             <div className="column">
-              <FilterDevelopers updateFilters={updateFilters} />
+              <MultiSelectFilter
+                updateFilters={updateFilters}
+                selectData={developersList}
+                paramName="param_z_developers"
+                placeholder="Developers" />
             </div>
             <div className="column">
-              <FilterFileTypes updateFilters={updateFilters} />
+              <MultiSelectFilter
+                updateFilters={updateFilters}
+                selectData={fileTypesList}
+                paramName="param_z_fileTypes"
+                placeholder="File Types" />
             </div>
             <div className="column">
-              <FilterDateRange updateFilters={updateFilters} />
+              <DateRangeFilter
+                updateFilters={updateFilters}
+                paramStartDate="param_z_date_start"
+                paramEndDate="param_z_date_end" />
             </div>
           </div>
         </div>
