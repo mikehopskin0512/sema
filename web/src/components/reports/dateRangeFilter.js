dadf0a9f7f9ae6542186ed558176724209f25358
Justin Ramers <jramers@semasoftware.com>
2020-03-21 16:09:46 -0700

    Set default values in components based on currentFilters in state

--- web/src/components/reports/dateRangeFilter.js
+++ web/src/components/reports/dateRangeFilter.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { format, subDays } from 'date-fns';
+import { format, parseISO, subDays } from 'date-fns';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './style.scss';
@@ -9,10 +9,11 @@ const today = new Date();
 const weekAgo = subDays(today, 7);
 
 const DateRangeFilter = (props) => {
-  const [startDate, _setStartDate] = useState(weekAgo);
-  const [endDate, _setEndDate] = useState(today);
-
-  const { updateFilters, paramStartDate, paramEndDate } = props;
+  const { updateFilters, currentFilters, paramStartDate, paramEndDate } = props;
+  const currentStartDate = parseISO(currentFilters[paramStartDate]);
+  const currentEndDate = parseISO(currentFilters[paramEndDate]);
+  const [startDate, _setStartDate] = useState(currentStartDate || weekAgo);
+  const [endDate, _setEndDate] = useState(currentEndDate || today);
 
   // Update report url when filters change
   const setStartDate = (date) => {
@@ -47,6 +48,7 @@ const DateRangeFilter = (props) => {
 
 DateRangeFilter.propTypes = {
   updateFilters: PropTypes.func.isRequired,
+  currentFilters: PropTypes.object.isRequired,
   paramStartDate: PropTypes.string.isRequired,
   paramEndDate: PropTypes.string.isRequired,
 };
