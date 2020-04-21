1564210b78b885d9e48797eb8307f3e759d3049f
Justin Ramers <jramers@semasoftware.com>
2020-04-20 19:36:10 -0700

    Add new API call for getPdfFile

--- web/src/state/features/reports/api.js
+++ web/src/state/features/reports/api.js
@@ -1,4 +1,5 @@
-import { getAll } from '../../utils/api';
+import { getAll, getDownload } from '../../utils/api';
 
 export const getReportUrl = (params, token) => getAll('/v1/embedded_bi', { params }, token);
-export const getPdfUrl = (token) => getAll('/v1/embedded_bi_pdf', {}, token);
+export const getRunToken = (reportId, token) => getAll(`/v1/embedded_bi/${reportId}/pdf`, {}, token);
+export const getPdfFile = (reportId, modeToken, token) => getDownload(`/v1/embedded_bi/${reportId}/export/${modeToken}/pdf`, {}, token);
