e058ebd34a02485bd11544d641b888aacbd25f1e
Justin Ramers <jramers@semasoftware.com>
2020-04-24 11:19:55 -0700

    Repurpose index page for reports list

--- web/src/pages/reports/index.js
+++ web/src/pages/reports/index.js
@@ -1,35 +1,16 @@
-import { useEffect, useState, useRef } from 'react';
+import Link from 'next/link';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import _ from 'lodash';
+
 import withLayout from '../../components/layout';
-import ReportsHeader from '../../components/reports/reportsHeader';
 
 import { reportsOperations } from '../../state/features/reports';
-import { organizationsOperations } from '../../state/features/organizations';
 
-const { fetchReportUrl } = reportsOperations;
-const { updateFilters } = organizationsOperations;
-
-// Hard code reportId
-const reportId = '43e7fa173112';
-
-const buildFilterUrl = (params) => _.map(params, (value, param) => {
-  if (_.isArray(value)) {
-    if (value.length > 0) {
-      // Array with values
-      const paramValues = _.map(value, (item) => `${param}%5B%5D=${encodeURIComponent(item)}`).join('&');
-      return paramValues;
-    }
-    // Array empty values
-    return `${param}=all`;
-  }
-  // String
-  return `${param}=${value}`;
-}).join('&');
+const { fetchReportList } = reportsOperations;
 
 const Reports = () => {
-  const reportFrame = useRef(null);
   const dispatch = useDispatch();
+  const spaceId = 'ff5e7381bd72';
 
   // Import Redux vars
   const { auth, reports, organizations } = useSelector(
@@ -40,50 +21,24 @@ const Reports = () => {
     }),
   );
 
-  // Get filters from Redux
-  const { currentFilters } = organizations;
+  // Get reportList from Redux state
+  const { reportList = [] } = reports;
 
-  // Declare local state var for report filterUrl
-  const [filterUrl, setFilterUrl] = useState(buildFilterUrl(currentFilters));
-
-  const handleUpdateFilters = (paramType, paramList) => {
-    const newFilters = {
-      ...currentFilters,
-      [paramType]: paramList,
-    };
-    dispatch(updateFilters(newFilters));
-  };
-
-  // Update report url when currentFilters change
+  // Fetch reports list from Mode space
   useEffect(() => {
-    setFilterUrl(buildFilterUrl(currentFilters));
-  }, [currentFilters]);
+    dispatch(fetchReportList(spaceId, auth.token));
+  }, [dispatch, spaceId, auth.token]);
 
-  // Get mode Url from embedded_bi endpoint
-  useEffect(() => {
-    dispatch(fetchReportUrl(reportId, filterUrl, auth.token));
-  }, [dispatch, filterUrl, auth.token]);
-
-  const { reportUrl } = reports;
+  const reportsStuff = reportList.map((report) => (
+    <p key={report.token}><Link href={`/reports/${report.token}`}>{report.name}</Link></p>
+  ));
 
   return (
     <div>
-      <ReportsHeader updateFilters={handleUpdateFilters} />
       <section className="section">
         <div className="container">
-          <h2 className="subtitle">
-            <figure className="image is-16by9">
-              <iframe
-                className="has-ratio"
-                ref={reportFrame}
-                key={reportUrl}
-                src={reportUrl}
-                title="Report frame"
-                frameBorder="0"
-                allowFullScreen />
-            </figure>
-          </h2>
-          <p>Debug: {reportUrl}</p>
+          <h2 className="subtitle">Reports</h2>
+          {reportsStuff}
         </div>
       </section>
     </div>
