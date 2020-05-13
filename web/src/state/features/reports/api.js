42f5849f0b0cb51cf7b1a26507aa6db16b0ba3b3
Justin Ramers <jramers@semasoftware.com>
2020-05-13 09:38:14 -0700

    Rename API call to generatePdf

--- web/src/state/features/reports/api.js
+++ web/src/state/features/reports/api.js
@@ -1,6 +1,6 @@
 import { getAll, getDownload } from '../../utils/api';
 
 export const getReportUrl = (params, token) => getAll('/v1/embedded_bi', { params }, token);
-export const getRunToken = (reportId, token) => getAll(`/v1/embedded_bi/${reportId}/pdf`, {}, token);
+export const generatePdf = (reportId, token) => getAll(`/v1/embedded_bi/${reportId}/pdf`, {}, token);
 export const getPdfFile = (reportId, modeToken, token) => getDownload(`/v1/embedded_bi/${reportId}/export/${modeToken}/pdf`, {}, token);
 export const getReportList = (spaceId, token) => getAll(`/v1/embedded_bi/space/${spaceId}`, {}, token);
