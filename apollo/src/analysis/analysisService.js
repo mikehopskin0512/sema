492709c2a84d5b52fb5a5d54966c290f3d49337e
Justin Ramers <jramers@semasoftware.com>
2020-12-07 09:14:56 -0800

    Add analysis endpoint and service

---
+++ apollo/src/analysis/analysisService.js
@@ -0,0 +1,86 @@
+import mongoose from 'mongoose';
+import { createAppAuth } from '@octokit/auth';
+import { getAppRepos } from '../identity/github/utils';
+import { github } from '../config';
+
+import logger from '../shared/logger';
+import errors from '../shared/errors';
+
+const {
+  Types: { ObjectId },
+} = mongoose;
+
+export const create = async (source) => {
+  try {
+    const { orgId, type } = source;
+
+    const query = Source.findOneAndUpdate(
+      { orgId, type },
+      { $set: source },
+      { upsert: true, new: true, setDefaultsOnInsert: true },
+    );
+    const updatedSource = await query.exec();
+
+    return updatedSource;
+  } catch (err) {
+    const error = new errors.BadRequest(err);
+    logger.error(error);
+    throw error;
+  }
+};
+
+export const find = async (id) => {
+  try {
+    const query = Source.findOne({ _id: new ObjectId(id) });
+    const source = await query.lean().exec();
+
+    return source;
+  } catch (err) {
+    logger.error(err);
+    const error = new errors.NotFound(err);
+    return error;
+  }
+};
+
+export const findByOrg = async (orgId) => {
+  try {
+    const query = Source.find({ orgId });
+    const sources = await query.lean().exec();
+
+    return sources;
+  } catch (err) {
+    logger.error(err);
+    const error = new errors.NotFound(err);
+    return error;
+  }
+};
+
+export const fetchRepositoriesGithub = async (externalSourceId) => {
+  try {
+    const auth = createAppAuth({
+      clientId: github.clientId,
+      clientSecret: github.clientSecret,
+      id: github.appId,
+      installationId: externalSourceId,
+      privateKey: github.privateKey,
+    });
+
+    const { token } = await auth({ type: 'installation' });
+
+    // Note: response from Github contains count, selection and repos array
+    const { repositories } = await getAppRepos(token);
+
+    if (!repositories) {
+      throw new errors.NotFound(
+        `No repositories found for installationId ${externalSourceId}`,
+      );
+    }
+
+    return repositories;
+  } catch (err) {
+    // Do not return error, instead throw
+    throw new errors.BadRequest(
+      `Error fetching repositories for installationId ${externalSourceId}`,
+    );
+  }
+};
---
+++ apollo/src/analysis/index.js
@@ -0,0 +1,49 @@
+import { Router } from 'express';
+import { version } from '../config';
+import logger from '../shared/logger';
+import errors from '../shared/errors';
+
+import {
+  create, find,
+} from './analysisService';
+
+const route = Router();
+
+export default (app, passport) => {
+  app.use(`/${version}/analysis`, route);
+
+  route.post('/', passport.authenticate(['bearer'], { session: false }), async (req, res) => {
+    const { source } = req.body;
+
+    try {
+      const newSource = await create(source);
+      if (!newSource) {
+        throw new errors.BadRequest('Source create error');
+      }
+
+      return res.status(201).send({
+        source: newSource,
+      });
+    } catch (error) {
+      logger.error(error);
+      return res.status(error.statusCode).send(error);
+    }
+  });
+
+  route.get('/', passport.authenticate(['bearer'], { session: false }), async (req, res) => {
+    const { orgId } = req.query;
+
+    try {
+      const sources = await findByOrg(orgId);
+      if (!sources) { throw new errors.NotFound('No sources found for this organization'); }
+
+      return res.status(201).send({
+        sources,
+      });
+    } catch (error) {
+      logger.error(error);
+      return res.status(error.statusCode).send(error);
+    }
+  });
+
+};
