81bb0cb6e3528f178895ec305d1b08b559424f7a
Brendan Cody-Kenny <brendan@semasoftware.com>
2020-12-09 20:11:09 +0000

    filtering blame to subdirectories or individual files within repo

--- athena/momus/get_blame.py
+++ athena/momus/get_blame.py
@@ -26,14 +26,14 @@ import datetime
 
 # Shell commands
 def run_command(command):
-    return subprocess.check_output(command).decode()
+    return subprocess.check_output(command).decode().strip()
 
 
 def run_command_read_lines(cmd):
     # https://stackoverflow.com/a/28995273/771112
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     for line in proc.stdout:
-        yield line.decode()  # stream lines as we receive them
+        yield line.decode().strip()  # stream lines as we receive them
 
 
 # Blame
@@ -50,11 +50,22 @@ def get_blame(config):
         r".*\.js$|.*\.py$|.*\.java$|.*\.c$|.*\.cc$|.*\.cpp$|.*\.cxx$|.*\.h$|.*\.hpp$|.*\.hxx$|.*\.swift$|.*\.ts$|.*\.rb$|.*\.php$|.*\.scala$|.*\.cs$"
     )
     auth_ln_re = re.compile(
-        r"[0-f]{40} .* \((?P<author_name>.*) [0-9]{4}-[0-9]{2}-[0-9]{2} *(?P<line_num>[0-9]*)\).* "
+        r"[0-f]{40} .* \((?P<author_name>.*) [0-9]{4}-[0-9]{2}-[0-9]{2} *(?P<line_num>[0-9]*)\).*"
     )
+    blamed_file_count = 0
+    file_count = 0
     for filename in get_files(config):
-        fn = filename.strip()
+        file_count += 1
+        if not filename.startswith(config.file_filter):
+            continue
+        fn = filename
+        if fn.startswith(config.repo_dir):
+            file_path = fn[len(config.repo_dir) :]
+        else:
+            file_path = fn
+
         if fe_re.match(fn):
+            blamed_file_count += 1
             for blame_line in run_command_read_lines(
                 [
                     "git",
@@ -71,10 +82,11 @@ def get_blame(config):
             ):
                 for match in re.finditer(auth_ln_re, blame_line):
                     yield {
-                        "file_path": fn,
-                        "author_name": match.group("author_name").strip(),
+                        "file_path": file_path,
+                        "author_name": match.group("author_name"),
                         "line_num": match.group("line_num"),
                     }
+    print("Blamed " + str(blamed_file_count) + " / " + str(file_count) + " files")
 
 
 def get_blame_short(config):
@@ -183,8 +195,8 @@ def parse_args():
     )
 
     parser.add_argument(
-        "repo_dir",
-        help="Directory which holds target git repo where blame will be read from",
+        "blame_target",  # repo_dir, a sub-dir within the repo dir, or a specific file
+        help="File or Directory that blame will be read for",
     )
 
     parser.add_argument(
@@ -204,21 +216,47 @@ def parse_args():
 
     args = parser.parse_args()
 
+    if os.path.isfile(args.blame_target):
+        args.blame_target_file = args.blame_target
+        args.repo_dir = run_command(
+            [
+                "git",
+                "-C",
+                os.path.dirname(args.blame_target),
+                "rev-parse",
+                "--show-toplevel",
+            ]
+        )
+    else:
+        args.repo_dir = run_command(
+            ["git", "-C", args.blame_target, "rev-parse", "--show-toplevel"]
+        )
+
     if not os.path.exists(args.repo_dir):
         print(
             "Error: the repo directory does not exist, expecting something like /src/customer-repos/Demo/locoGP"
         )
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), args.repo_dir)
 
+    # if a sub-dir of the toplevel repo dir is passed in
+    args.file_filter = args.blame_target[len(args.repo_dir) :]
+    while args.file_filter.startswith(os.sep):
+        args.file_filter = args.file_filter[1:]
+    while args.file_filter.endswith(os.sep):
+        args.file_filter = args.file_filter[:-1]
+
+    print("Repo dir: " + args.repo_dir)
+    print("Will get blame for: " + args.blame_target)
+
     # if no project or org name set, work it out from the URL
     if not args.org_name:
-        args.org_name = urlparse(args.repo_dir).path.split(os.sep)[-1]
+        args.org_name = urlparse(args.repo_dir).path.split(os.sep)[-2]
     if not args.project_name:
-        args.project_name = urlparse(args.repo_dir).path.split(os.sep)[-2]
+        args.project_name = urlparse(args.repo_dir).path.split(os.sep)[-1]
 
     args.commit_hash = run_command(
         ["git", "-C", args.repo_dir, "rev-parse", "HEAD"]
-    ).strip()
+    )
 
     args.commit_timestamp = datetime.datetime.strptime(
         run_command(
@@ -232,7 +270,7 @@ def parse_args():
                 "--date=iso",
                 "HEAD",
             ]
-        ).strip(),
+        ),
         "%Y-%m-%d %H:%M:%S %z",
     )  # Wed Nov 18 18:10:15 2020 +0000
 
