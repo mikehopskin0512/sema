972615fa168b784c4671c2b4c7f5c8753d716229
Justin Ramers <jramers@semasoftware.com>
2020-12-18 10:54:15 -0800

    Check for legacyId and fetch repo if not present

--- apollo/src/analysis/index.js
+++ apollo/src/analysis/index.js
@@ -7,6 +7,7 @@ import errors from '../shared/errors';
 import { findByProject, createRun, updateProjectWithCreds } from '../shared/scqp';
 import { fetchGithubToken } from '../identity/github/utils';
 import { create, sendNotification } from './analysisService';
+import { get as getRepository } from '../repositories/repositoriesService';
 
 const route = Router();
 
@@ -14,9 +15,18 @@ export default (app, passport) => {
   app.use(`/${version}/analysis`, route);
 
   route.post('/', passport.authenticate(['bearer'], { session: false }), async (req, res) => {
-    const { repositoryId, legacyId, externalSourceId } = req.body;
+    const { repositoryId, externalSourceId } = req.body;
+    let { legacyId } = req.body;
 
     try {
+      // Check for legacyId or fetch if missing
+      if (!legacyId) {
+        ({ legacyId } = getRepository(repositoryId));
+        if (!legacyId) {
+          throw new errors.BadRequest('Unable to fetch legacyId. Analysis aborted.');
+        }
+      }
+
       // Check if run is in progress
       const existingRuns = await findByProject(legacyId);
       if (existingRuns && existingRuns.length > 0) {
