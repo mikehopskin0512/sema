09e7c8f7566d7297da34ddc2b96f25e3038e5953
Brendan Cody-Kenny <codykenny@gmail.com>
2020-12-22 17:37:47 +0000

    SP-498: Ananke implementation (current file summary rewrite)
    * Initial implementation of wc and mime to csv
    
    * created a shared library, which allows adding any cli tool and any regex to parse output
    
    * wc, mime and language (from enry) can be read to csv
    
    * Added docker config files for ananke (currentfilessummary)
    
    * adding ananke to docker compose
    
    * ran code formatter on python code (black)
    
    * retrieving enry download

---
+++ athena/ananke/get_enry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+""" 
+
+Call file to get mime
+
+"""
+
+import get_cli_csv
+
+
+def main():
+    config = type("", (), {})
+    # should we just have get_cli read this from config?
+    config.tool_name = "enry"
+
+    """ 
+    enry-v1.1.0-linux-amd64.tar.gz: 11425 lines (11396 sloc)
+    type:      Binary
+    mime_type: text/plain
+    language:  
+    vendored:  false
+    """
+    config.data_row_match_regex = r".*: (?P<line_count>[0-9]+) lines \((?P<sloc_count>[0-9]+) sloc\) *$.* *type: *(?P<file_type>[^\s]*)\s*language: *(?P<language>[^\s]*)\s*vendored: *(?P<vendored>[^\s]*)"
+
+    config.cli_command = ["enry", "x-filename-placeholder-x"]
+
+    get_cli_csv.get_cmd_csv(config)
+
+
+if __name__ == "__main__":
+    main()
