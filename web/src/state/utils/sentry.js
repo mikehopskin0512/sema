ac8d7b81df0cc8488ab0be242f92e58d00d9d10c
Brendan <codykenny@gmail.com>
2021-03-12 20:34:06 +0000

    remove debug console logs

--- web/src/state/utils/sentry.js
+++ web/src/state/utils/sentry.js
@@ -12,9 +12,7 @@ const isServer = () => typeof window === 'undefined';
 const redirect = (ctx, location) => {
   if (ctx.req) {
     ctx.res.writeHead(302, { Location: location }).end();
-    // ctx.res.end();
   } else {
-    console.log(`actually, let's go to ${location}`);
     Router.push(location);
   }
 };
@@ -23,22 +21,16 @@ const initialize = async (ctx) => {
   let jwt = null;
   let isVerified = false;
   if (isServer()) {
-    console.log("isServer");
     // On server-side, get refreshCookie and call refreshJwt (which will return isVerified)
     jwt = getCookie(refreshCookie, ctx.req);
     const payload = (jwt) ? await ctx.store.dispatch(refreshJwt(jwt)) : {};
-    console.log({payload});
     ({ isVerified } = payload);
   } else {
-    console.log("not isServer");
     // On client-side, get jwt from state and decode to get isVerified
     ({ authState: { token: jwt } } = ctx.store.getState());
     const { user = {} } = (jwt) ? jwtDecode(jwt) : {};
     ({ isVerified } = user);
   }
-
-  console.log({ jwt_in_initialize: jwt });
-  console.log({ isVerified_in_initialize: payload.isVerified });
 
   // Redirects w/ exclusions
   if (ctx.pathname !== '/login' &&
