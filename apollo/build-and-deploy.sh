d3afab98b1b2335a6a77f2eeff7a15411c1013b5
Kevin Castro <me@kevincastro.dev>
2021-03-12 16:35:17 -0800

    create test build pipeline & improve image tagging

--- apollo/build-and-deploy.sh
+++ apollo/build-and-deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-CLUSTER_NAME=qa1-frontend
+ECR_URL=091235034633.dkr.ecr.us-east-1.amazonaws.com
+ENV=qa1
+CLUSTER_NAME=$ENV-frontend
 SERVICE_NAME=apollo
+TASK_FAMILY_NAME=$ENV-$SERVICE_NAME
 
 NAME=apollo
 NODE_ENV=staging
@@ -8,6 +11,8 @@ DOCKER_FILE=../.docker/apollo/Dockerfile.prod
 BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 SHA1=$(git rev-parse HEAD)
 VERSION=$BRANCH-$SHA1-$NODE_ENV
+
+IMAGE=$ECR_URL/$NAME:$VERSION
 
 aws configure set default.region us-east-1
 
@@ -18,9 +23,25 @@ echo "Building image..."
 sudo docker build -f $DOCKER_FILE -t $NAME:$VERSION . --no-cache
 #docker build -t $NAME:$VERSION .
 echo "Tagging image..."
-sudo docker tag $NAME:$VERSION 091235034633.dkr.ecr.us-east-1.amazonaws.com/$NAME:$VERSION
+sudo docker tag $NAME:$VERSION $IMAGE
 echo "Pushing image..."
-sudo docker push 091235034633.dkr.ecr.us-east-1.amazonaws.com/$NAME:$VERSION
+sudo docker push $IMAGE
 
+# get the latest task definition
+LATEST_TASK_DEFINITION=aws ecs describe-task-definition --task-definition $TASK_FAMILY_NAME
+
+# get the latest task definition ARN
+LATEST_TASK_DEFINITION_ARN=$(echo $LATEST_TASK_DEFINITION | jq ".taskDefinition.taskDefinitionArn")
+
+# update the first container in the containerDefinitions with the new image
+CONTAINER_DEFINITIONS=$(echo $LATEST_TASK_DEFINITION | jq ".taskDefinition.containerDefinitions[0].image = "$IMAGE" | [.containerDefinitions]")
+
+# register the new task definition
+aws ecs register-task-definition --family $TASK_FAMILY_NAME --container-definitions $CONTAINER_DEFINITIONS
+
+# de-register the old task definition
+aws ecs deregister-task-definition --task-definition $LATEST_TASK_DEFINITION_ARN
+
+# force update and redeploy
 echo "Updating ECS..."
 aws --profile phoenix  ecs update-service --force-new-deployment --cluster $CLUSTER_NAME --service $SERVICE_NAME > /dev/null
