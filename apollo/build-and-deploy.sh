b3d6b26b62b7381e4a46bf55ec8ba26ba43e66d9
Kevin Castro <me@kevincastro.dev>
2021-03-13 09:53:46 -0800

    update web and apollo with working deploy script

--- apollo/build-and-deploy.sh
+++ apollo/build-and-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-ECR_URL=091235034633.dkr.ecr.us-east-1.amazonaws.com
-ENV=qa1
-CLUSTER_NAME=$ENV-frontend
-SERVICE_NAME=apollo
-TASK_FAMILY_NAME=$ENV-$SERVICE_NAME
-
 NAME=apollo
+ENV=qa1
+
+ECR_URL=091235034633.dkr.ecr.us-east-1.amazonaws.com
+CLUSTER_NAME=$ENV-frontend
+TASK_FAMILY_NAME=$ENV-$NAME
+
 NODE_ENV=staging
 DOCKER_FILE=../.docker/apollo/Dockerfile.prod
 BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
@@ -30,23 +30,15 @@ sudo docker push $IMAGE
 echo "creating new task definition with image..."
 # get the latest task definition
 LATEST_TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY_NAME)
-# get the latest task definition ARN and execution role ARN
-LATEST_TASK_DEFINITION_ARN=$(echo $LATEST_TASK_DEFINITION | jq -r ".taskDefinition.taskDefinitionArn")
-LATEST_TASK_EXECUTION_ROLE_ARN=$(echo $LATEST_TASK_DEFINITION | jq -r ".taskDefinition.executionRoleArn")
-# get the latest task definition excution ARN (needed for using secrets from the parameter store)
-LATEST_TASK_EXECUTION_ROLE_ARN=$(echo $LATEST_TASK_DEFINITION | jq -r ".taskDefinition.executionRoleArn")
 # update the first container in the containerDefinitions with the new image
+# remove unallowed values received from latest task defintion for new task creation
 # we only have 1 container per task definition currently
-CONTAINER_DEFINITIONS=$(echo $LATEST_TASK_DEFINITION | jq ".taskDefinition.containerDefinitions[0].image = \"$IMAGE\" | .taskDefinition.containerDefinitions")
+NEW_TASK_DEFINITION=$(echo $LATEST_TASK_DEFINITION | jq ".taskDefinition.containerDefinitions[0].image = \"$IMAGE\" | del(.taskDefinition.taskDefinitionArn, .taskDefinition.revision, .taskDefinition.status, .taskDefinition.requiresAttributes, .taskDefinition.compatibilities) | .taskDefinition")
 # register the new task definition
-NEW_TASK_DEFINITION_ARN=$(aws ecs register-task-definition --family $TASK_FAMILY_NAME --container-definitions "$CONTAINER_DEFINITIONS" --execution-role-arn $LATEST_TASK_EXECUTION_ROLE_ARN | jq -r ".taskDefinition.taskDefinitionArn")
+NEW_TASK_DEFINITION_ARN=$(aws ecs register-task-definition --cli-input-json "$NEW_TASK_DEFINITION" | jq -r ".taskDefinition.taskDefinitionArn")
 
-echo "making old task definition inactive..."
-# de-register the old task definition
-aws ecs deregister-task-definition --task-definition $LATEST_TASK_DEFINITION_ARN > /dev/null
-
-# force update to use new task definition and redeploy
 echo "Updating ECS with new task definition..."
-aws --profile phoenix ecs update-service --force-new-deployment --cluster $CLUSTER_NAME --service $SERVICE_NAME --task-definition $NEW_TASK_DEFINITION_ARN > /dev/null
+# force update to use new task definition and redeploy
+aws --profile phoenix ecs update-service --force-new-deployment --cluster $CLUSTER_NAME --service $NAME --task-definition $NEW_TASK_DEFINITION_ARN > /dev/null
 
 echo "done :)"
--- web/build-and-deploy.sh
+++ web/build-and-deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-CLUSTER_NAME=qa1-frontend
-SERVICE_NAME=phoenix
-
 NAME=phoenix
+ENV=qa1
+
+ECR_URL=091235034633.dkr.ecr.us-east-1.amazonaws.com
+CLUSTER_NAME=$ENV-frontend
+TASK_FAMILY_NAME=$ENV-$NAME
+
 NODE_ENV=staging
 DOCKER_FILE=../.docker/web/Dockerfile.prod
 BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 SHA1=$(git rev-parse HEAD)
 VERSION=$BRANCH-$SHA1-$NODE_ENV
+
+IMAGE=$ECR_URL/$NAME:$VERSION
 
 aws configure set default.region us-east-1
 
@@ -18,9 +23,22 @@ echo "Building image..."
 sudo docker build -f $DOCKER_FILE -t $NAME:$VERSION . --no-cache
 #docker build -t $NAME:$VERSION .
 echo "Tagging image..."
-sudo docker tag $NAME:$VERSION 091235034633.dkr.ecr.us-east-1.amazonaws.com/$NAME:$VERSION
+sudo docker tag $NAME:$VERSION $IMAGE
 echo "Pushing image..."
-sudo docker push 091235034633.dkr.ecr.us-east-1.amazonaws.com/$NAME:$VERSION
+sudo docker push $IMAGE
 
-echo "Updating ECS..."
-aws --profile phoenix  ecs update-service --force-new-deployment --cluster $CLUSTER_NAME --service $SERVICE_NAME > /dev/null
+echo "creating new task definition with image..."
+# get the latest task definition
+LATEST_TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY_NAME)
+# update the first container in the containerDefinitions with the new image
+# remove unallowed values received from latest task defintion for new task creation
+# we only have 1 container per task definition currently
+NEW_TASK_DEFINITION=$(echo $LATEST_TASK_DEFINITION | jq ".taskDefinition.containerDefinitions[0].image = \"$IMAGE\" | del(.taskDefinition.taskDefinitionArn, .taskDefinition.revision, .taskDefinition.status, .taskDefinition.requiresAttributes, .taskDefinition.compatibilities) | .taskDefinition")
+# register the new task definition
+NEW_TASK_DEFINITION_ARN=$(aws ecs register-task-definition --cli-input-json "$NEW_TASK_DEFINITION" | jq -r ".taskDefinition.taskDefinitionArn")
+
+echo "Updating ECS with new task definition..."
+# force update to use new task definition and redeploy
+aws --profile phoenix ecs update-service --force-new-deployment --cluster $CLUSTER_NAME --service $NAME --task-definition $NEW_TASK_DEFINITION_ARN > /dev/null
+
+echo "done :)"
