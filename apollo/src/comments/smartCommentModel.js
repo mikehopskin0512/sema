acb20c93ec4c0522d76366808bee7848a839f37c
mazzonig20 <guillermo@estoca.com.br>
2021-04-15 16:23:29 -0600

    Fixing typo and introducing suggestedcomments

--- apollo/src/comments/smartCommentModel.js
+++ apollo/src/comments/smartCommentModel.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const smartCommentSchema = new Schema({
   comment: String,
   suggestedComments: [{ type: Schema.Types.ObjectId, ref: 'SuggestedComment' }],
-  reactions: { type: Schema.Types.ObjectId, ref: 'Reaction' },
+  reaction: { type: Schema.Types.ObjectId, ref: 'Reaction' },
   tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
 }, { timestamps: true });
 
