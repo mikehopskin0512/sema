efb86cccf05780986a725cdeedd49346d2d569dc
mazzonig20 <guillermo@estoca.com.br>
2021-04-26 09:52:46 -0600

    PHX-537: Added Github metada field/property to create endpoint

--- apollo/src/comments/commentService.js
+++ apollo/src/comments/commentService.js
@@ -34,13 +34,20 @@ const searchComments = async (searchQuery) => {
   return returnResults;
 };
 
-const create = async ({ comment = null, suggestedComments = null, reaction = null, tags = null }) => {
+const create = async ({
+  comment = null,
+  suggestedComments = null,
+  reaction = null,
+  tags = null,
+  githubMetada = null,
+}) => {
   try {
     const smartComment = new SmartComment();
     smartComment.comment = comment;
     smartComment.suggestedComments = suggestedComments;
     smartComment.reaction = reaction;
     smartComment.tags = tags;
+    smartComment.githubMetada = githubMetada;
     const savedSmartComment = await smartComment.save();
     return savedSmartComment;
   } catch (err) {
