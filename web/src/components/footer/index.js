206f4ecd9127e2f839f7bacd3844b1d47986c6b1
jaymendez <johnjosephmendez2016@gmail.com>
2021-04-29 01:42:01 +0800

    refactor

--- web/src/components/footer/index.js
+++ web/src/components/footer/index.js
@@ -7,49 +7,26 @@ const Footer = () => {
   const [termsAndConditionsLink] = useState('https://semasoftware.com/terms-and-conditions');
   const [semaEmail] = useState('feedback@semasoftware.com');
 
-  return (<footer className={styles.footer}>
-    <div className="columns is-vcentered is-centered">
-      <div className={clsx("column is-one-quarter-fullhd is-1-desktop", styles.tile)} />
-      <div className="column has-text-primary-light has-text-centered">
-        &copy; {new Date().getFullYear()} Sema
+  return (
+    <footer className={styles.footer}>
+      <div className="columns is-vcentered is-centered">
+        <div className={clsx('column is-one-quarter-fullhd is-1-desktop', styles.tile)} />
+        <div className="column has-text-primary-light has-text-centered">
+          &copy; {new Date().getFullYear()} Sema
+        </div>
+        <div className="column has-text-centered">
+          <a className="button is-ghost has-text-primary-light" href={dashboardLink}>Dashboard</a>
+        </div>
+        <div className="column has-text-centered">
+          <a className="button is-ghost has-text-primary-light" href={termsAndConditionsLink}>Terms and Conditions</a>
+        </div>
+        <div className="column has-text-centered">
+          <a className="button is-ghost has-text-primary-light is-size-m-desktop" href={`mailto:${semaEmail}?subject=Product Feedback`}>Send Feedback</a>
+        </div>
+        <div className="column is-one-quarter-fullhd is-1-desktop" />
       </div>
-      <div className="column has-text-centered">
-        <a className="button is-ghost has-text-primary-light" href={dashboardLink}>Dashboard</a>
-      </div>
-      <div className="column has-text-centered">
-        <a className="button is-ghost has-text-primary-light" href={termsAndConditionsLink}>Terms and Conditions</a>
-      </div>
-      <div className="column has-text-centered">
-        <a className="button is-ghost has-text-primary-light is-size-m-desktop" href={`mailto:${semaEmail}?subject=Product Feedback`}>Send Feedback</a>
-      </div>
-      <div className="column is-one-quarter-fullhd is-1-desktop" />
-    </div>
-  </footer>)
+    </footer>
+  );
 };
-
-// const Footer = () => {
-//   const [dashboardLink] = useState('https://app.semasoftware.com');
-//   const [termsAndConditionsLink] = useState('https://semasoftware.com/terms-and-conditions');
-//   const [semaEmail] = useState('feedback@semasoftware.com');
-
-//   return (
-//     <footer className={clsx('', styles.footer)}>
-//       <div className={clsx('tile is-ancestor is-align-items-center', styles.tile)}>
-//         <div className="tile is-child has-text-primary-light has-text-centered">
-//           &copy; {new Date().getFullYear()} Sema
-//         </div>
-//         <div className="tile is-child has-text-centered">
-//           <a className="button is-ghost has-text-primary-light" href={dashboardLink}>Dashboard</a>
-//         </div>
-//         <div className="tile is-child has-text-centered">
-//           <a className="button is-ghost has-text-primary-light" href={termsAndConditionsLink}>Terms and Conditions</a>
-//         </div>
-//         <div className="tile is-child has-text-centered">
-//           <a className="button is-ghost has-text-primary-light" href={`mailto:${semaEmail}?subject=Product Feedback`}>Send Feedback</a>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
 
 export default Footer;
