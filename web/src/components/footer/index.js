ab7bcb997d4dfe620c97760fee84cdae73003114
jaymendez <johnjosephmendez2016@gmail.com>
2021-04-29 01:24:27 +0800

    used column for footer to make it more response

--- web/src/components/footer/index.js
+++ web/src/components/footer/index.js
@@ -2,20 +2,22 @@ import clsx from 'clsx';
 import styles from './footer.module.scss';
 
 const Footer = () => (
-  <footer className={clsx('', styles.footer)}>
-    <div className={clsx('tile is-ancestor is-align-items-center', styles.tile)}>
-      <div className="tile is-child has-text-primary-light has-text-centered">
+  <footer className={styles.footer}>
+    <div className="columns is-vcentered is-centered">
+      <div className="column is-one-quarter-fullhd is-1-desktop" />
+      <div className="column has-text-primary-light has-text-centered">
         &copy; {new Date().getFullYear()} Sema
       </div>
-      <div className="tile is-child has-text-centered">
+      <div className="column has-text-centered">
         <a className="button is-ghost has-text-primary-light" href="#">Dashboard</a>
       </div>
-      <div className="tile is-child has-text-centered">
+      <div className="column has-text-centered">
         <a className="button is-ghost has-text-primary-light" href="#">Terms and Conditions</a>
       </div>
-      <div className="tile is-child has-text-centered">
-      <a className="button is-ghost has-text-primary-light" href="#">Send Feedback</a>
+      <div className="column has-text-centered">
+        <a className="button is-ghost has-text-primary-light is-size-m-desktop" href="#">Send Feedback</a>
       </div>
+      <div className="column is-one-quarter-fullhd is-1-desktop" />
     </div>
   </footer>
 );
