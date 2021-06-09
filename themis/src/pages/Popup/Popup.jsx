f77982392e9bc1fc63cb1df37d46753b17f614c3
Abhishek <abhishek@semasoftware.com>
2021-06-09 17:29:12 +0530

    open sema UI from popup

--- themis/src/pages/Popup/Popup.jsx
+++ themis/src/pages/Popup/Popup.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import codeIcon from '../../assets/img/codelines.png';
 import logo from '../../assets/img/sema-logo.png';
-// TODO: should include it in the project directly or make sema-custom at a common place for all?
-import '../Content/styles/sema-custom.css';
 import './Popup.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
-const isLoggedIn = false;
+const semaUIUrl = process.env.SEMA_UI_URL;
+
+const isLoggedIn = true;
+
+const openSema = () => {
+  window.open(semaUIUrl, '_blank');
+};
 
 const loggedInElem = (
   <div
@@ -26,21 +30,8 @@ const loggedInElem = (
         Login required to activate the Sema Chrome Extension
       </p>
       <img src={codeIcon} />
-      <button className="login-primary">
+      <button className="login-primary" onClick={openSema}>
         <FontAwesomeIcon icon={faGithub} className="github" />
-        {/* <svg
-          width="30"
-          height="29"
-          viewBox="0 0 30 29"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M14.9997 0C7.05438 0 0.608398 6.45309 0.608398 14.4072C0.608398 22.3613 7.05438 28.8144 14.9997 28.8144C22.9451 28.8144 29.3911 22.3613 29.3911 14.4072C29.3911 6.45309 22.9451 0 14.9997 0ZM7.91786 22.884L12.8779 4.44278H15.6356L10.6756 22.884H7.91786ZM23.755 16.1093L22.249 17.3289L19.8526 14.6082L17.3559 17.3289L15.7896 16.1763L17.7709 13.1675L14.5044 11.8876L15.0868 9.80361L18.534 10.7015L18.8218 7.27732H20.7764L21.0642 10.7015L24.3307 9.87062L25.0001 11.8876L21.767 13.1675L23.755 16.1093Z"
-            fill="white"
-          />
-        </svg> */}
-
         <span className="login-primary-content">
           Join our Waitlist with Github
         </span>
@@ -50,7 +41,7 @@ const loggedInElem = (
         <p className="already-account-content">Already have an account?</p>
         <span className="divider"></span>
       </div>
-      <button className="login-secondary">
+      <button className="login-secondary" onClick={openSema}>
         <FontAwesomeIcon icon={faGithub} className="github" />
         <span className="login-secondary-content">Sign in with Github</span>
       </button>
@@ -84,7 +75,7 @@ const loggedOutElem = (
         Hi Ed,
       </p>
       <img src={codeIcon} style={{ marginBottom: '47px' }} />
-      <button className="login-primary">
+      <button className="login-primary" onClick={openSema}>
         <svg
           width="30"
           height="29"
