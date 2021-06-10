31c780fbdadde0b2532450dc02a1fdb8fa7ba075
Xandy <xandy@semasoftware.com>
2021-06-10 19:01:41 +0800

    [PHX-787] Dashboard - Contact Us responsiveness

--- web/src/pages/dashboard/index.js
+++ web/src/pages/dashboard/index.js
@@ -411,7 +411,7 @@ const ContactUs = ({ userVoiceToken }) => {
       </div>
       {/* Mobile View */}
       { isMobile ? (
-        <div className="mt-20 p-25 columns has-background-primary is-centered is-vcentered is-hidden-desktop">
+        <div className="mt-20 p-25 has-background-primary is-hidden-desktop">
           <ContactUsContent {...userVoiceToken} />
         </div>
       ) : null }
