5492c444501ef7cbea4a42aec04a8e01971db9e6
Abhishek <abhishek@semasoftware.com>
2021-06-10 16:57:34 +0530

    added user details to popup

--- themis/src/pages/Popup/Popup.jsx
+++ themis/src/pages/Popup/Popup.jsx
@@ -5,12 +5,14 @@ import codeIcon from '../../assets/img/codelines.png';
 import logo from '../../assets/img/sema-logo.png';
 import lines from '../../assets/img/Lines.png';
 import './Popup.css';
+// TODO: how common files be shared across?
+import { WHOAMI } from '../Content/constants';
+import jwt_decode from 'jwt-decode';
 
 const semaUIUrl = process.env.SEMA_UI_URL;
 
 const checkLoggedIn = (cb) => {
-  chrome.runtime.sendMessage({ ['WHOAMI']: 'WHOAMI' }, function (response) {
-    console.log('resp', response);
+  chrome.runtime.sendMessage({ [WHOAMI]: WHOAMI }, function (response) {
     cb(response);
   });
 };
@@ -19,7 +21,7 @@ const openSema = () => {
   window.open(semaUIUrl, '_blank');
 };
 
-const loggedOutElem = (
+const loggedOutElem = () => (
   <div
     className="popup-container"
     style={{
@@ -55,60 +57,69 @@ const loggedOutElem = (
   </div>
 );
 
-const loggedInElem = (
-  <div
-    className="popup-container"
-    style={{
-      width: '500px',
-      height: '563px',
-      backgroundImage: `url(${lines})`,
-    }}
-  >
-    <div className="popup-header">
-      <img className="popup-logo" src={logo} />
-    </div>
+const loggedInElem = (userDetails) => {
+  const {
+    user: { firstName },
+  } = userDetails;
+  return (
     <div
-      className="popup-hero"
+      className="popup-container"
       style={{
-        justifyContent: 'start',
-        marginTop: '40px',
-        marginBottom: '60px',
+        width: '500px',
+        height: '563px',
+        backgroundImage: `url(${lines})`,
       }}
     >
-      <p
-        className="sema-login-details"
-        style={{ alignSelf: 'start', margin: '32px 0px 60px 34px' }}
+      <div className="popup-header">
+        <img className="popup-logo" src={logo} />
+      </div>
+      <div
+        className="popup-hero"
+        style={{
+          justifyContent: 'start',
+          marginTop: '40px',
+          marginBottom: '60px',
+        }}
       >
-        Hi Ed,
-      </p>
-      <img src={codeIcon} style={{ marginBottom: '47px' }} />
-      <button className="login-primary" onClick={openSema}>
-        <svg
-          width="30"
-          height="29"
-          viewBox="0 0 30 29"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
+        <p
+          className="sema-login-details"
+          style={{ alignSelf: 'start', margin: '32px 0px 60px 34px' }}
         >
-          <path
-            d="M14.9997 0C7.05438 0 0.608398 6.45309 0.608398 14.4072C0.608398 22.3613 7.05438 28.8144 14.9997 28.8144C22.9451 28.8144 29.3911 22.3613 29.3911 14.4072C29.3911 6.45309 22.9451 0 14.9997 0ZM7.91786 22.884L12.8779 4.44278H15.6356L10.6756 22.884H7.91786ZM23.755 16.1093L22.249 17.3289L19.8526 14.6082L17.3559 17.3289L15.7896 16.1763L17.7709 13.1675L14.5044 11.8876L15.0868 9.80361L18.534 10.7015L18.8218 7.27732H20.7764L21.0642 10.7015L24.3307 9.87062L25.0001 11.8876L21.767 13.1675L23.755 16.1093Z"
-            fill="white"
-          />
-        </svg>
-        <span className="login-primary-content">Visit the Sema Dashboard</span>
-      </button>
+          Hi {firstName},
+        </p>
+        <img src={codeIcon} style={{ marginBottom: '47px' }} />
+        <button className="login-primary" onClick={openSema}>
+          <svg
+            width="30"
+            height="29"
+            viewBox="0 0 30 29"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.9997 0C7.05438 0 0.608398 6.45309 0.608398 14.4072C0.608398 22.3613 7.05438 28.8144 14.9997 28.8144C22.9451 28.8144 29.3911 22.3613 29.3911 14.4072C29.3911 6.45309 22.9451 0 14.9997 0ZM7.91786 22.884L12.8779 4.44278H15.6356L10.6756 22.884H7.91786ZM23.755 16.1093L22.249 17.3289L19.8526 14.6082L17.3559 17.3289L15.7896 16.1763L17.7709 13.1675L14.5044 11.8876L15.0868 9.80361L18.534 10.7015L18.8218 7.27732H20.7764L21.0642 10.7015L24.3307 9.87062L25.0001 11.8876L21.767 13.1675L23.755 16.1093Z"
+              fill="white"
+            />
+          </svg>
+          <span className="login-primary-content">
+            Visit the Sema Dashboard
+          </span>
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 const Popup = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [loggedInDetails, setLoggedInDetails] = useState({});
 
   useEffect(() => {
-    checkLoggedIn(setLoggedIn);
-  });
+    checkLoggedIn(setLoggedInDetails);
+  }, [loggedInDetails?.token]);
 
-  return isLoggedIn ? loggedInElem : loggedOutElem;
+  const { token, isLoggedIn } = loggedInDetails;
+
+  return isLoggedIn ? loggedInElem(jwt_decode(token)) : loggedOutElem();
 };
 
 export default Popup;
