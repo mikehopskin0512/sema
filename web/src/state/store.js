329a1acd67f532474c1ad66c878b9ca72723f451
mazzonig20 <guillermo@estoca.com.br>
2021-06-11 07:19:33 -0600

    PHX-772: Fixed accessToken auto renewal

--- web/src/state/store.js
+++ web/src/state/store.js
@@ -2,7 +2,7 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import * as reducers from './features';
-import { apiMiddleware } from './middleware/api';
+import { setAxiosInterceptor } from './utils/api';
 
 const middleware = [thunk];
 
@@ -18,9 +18,12 @@ const middleware = [thunk];
 
 export const initStore = (initialState = {}) => {
   const rootReducer = combineReducers(reducers);
-  return createStore(
+  const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(...middleware, apiMiddleware)),
+    composeWithDevTools(applyMiddleware(...middleware)),
   );
+  
+  setAxiosInterceptor(store);
+  return store;
 };
