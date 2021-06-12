5407146fef91b64873d0f5d5a8dcec57486bc028
Justin Ramers <jramers@semasoftware.com>
2021-06-12 16:23:40 -0700

    Add updateUser functions, api call, reducers, & types

--- web/src/state/features/auth/api.js
+++ web/src/state/features/auth/api.js
@@ -1,8 +1,9 @@
-import { getAll, create, update } from '../../utils/api';
+import { getAll, create, update, updateItem } from '../../utils/api';
 
 export const auth = (params) => create('/api/proxy/auth/token', params);
 export const exchangeToken = (params) => create('/api/proxy/auth/refresh-token', params);
 export const createUser = (params) => create('/api/proxy/users', params);
+export const putUser = (id, item, token) => updateItem(`/api/proxy/users`, id, item, token);
 export const verifyUser = (params, token) => getAll(`/api/proxy/users/verification/${token}`, params);
 export const resetVerification = (params) => create('/api/proxy/users/verification', params);
 export const postUserOrg = (userId, params, token) => create(`/api/proxy/users/${userId}/organizations`, params, token);
