c5c6dc756f8201f8283ba7686292ce7bcd8421da
John Joseph Mendez <jaymendez>
2021-06-23 23:04:46 +0800

    sidenav for activity logs

---
+++ web/src/pages/logs/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from "../../components/sidebar";
+import withLayout from '../../components/layout';
+
+const ActivityLogs = () => {
+  return (
+    <div>
+      <Sidebar>
+        Test
+      </Sidebar>
+    </div>
+  )
+}
+
+export default withLayout(ActivityLogs);
