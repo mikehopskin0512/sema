91e8b086e4d89ff97cdcb5b349d463528d2186b7
John Joseph Mendez <jaymendez>
2021-07-07 20:57:48 +0800

    Fix for filter sema repos

--- web/src/state/features/repositories/api.js
+++ web/src/state/features/repositories/api.js
@@ -3,4 +3,4 @@ import { getAll, create } from '../../utils/api';
 export const postRepositories = (params, token) => create('/api/proxy/repositories', params, token);
 export const getRepos = (params, token) => getAll('/api/proxy/repositories', { params }, token);
 export const postAnalysis = (params, token) => create('/api/proxy/analysis', params, token);
-export const filterSemaRepos = (params, token) => getAll('/api/proxy/repositories/sema-repositories', params, token);
+export const filterSemaRepos = (params, token) => getAll('/api/proxy/repositories/sema-repositories', { params }, token);
