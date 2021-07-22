cfe5f88eeb7dbc4d6feea56e8fd1b038e53fd499
Xandy <xandy@semasoftware.com>
2021-07-22 21:18:51 +0800

    [PHX-1117] Integration and filters

--- web/src/components/comment/cardList/index.js
+++ web/src/components/comment/cardList/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CollectionCard from '../collectionCard';
 
-const CardList = () => (
-  <div className="is-flex is-justify-content-flex-start is-flex-wrap-wrap">
-    <CollectionCard name="Card Name 1" />
-    <CollectionCard name="Card Name 2" />
-    <CollectionCard name="Card Name 3" />
-    <CollectionCard name="Card Name 4" />
-    <CollectionCard name="Card Name 5" />
-  </div>
+const CardList = ({ collections }) => (
+  collections.length && collections.length > 0 ? (
+    <div className="is-flex is-justify-content-flex-start is-flex-wrap-wrap">
+      {collections.map((item) => (<CollectionCard {...item} key={`${item.collectionData._id}-collection`} />))}
+    </div>
+  ) :
+    <p className="px-10 py-20 mb-50">No collections!</p>
 );
+
+CardList.propTypes = {
+  collections: PropTypes.array.isRequired,
+};
 
 export default CardList;
--- web/src/components/comment/collectionCard/index.js
+++ web/src/components/comment/collectionCard/index.js
@@ -3,7 +3,9 @@ import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import styles from './collectionCard.module.scss';
 
-const CollectionCard = ({ name }) => {
+const CollectionCard = ({ isActive, collectionData }) => {
+  const { name, description, comments = [] } = collectionData;
+
   const renderStats = (label, value) => (
     <div className={clsx(
       'has-background-gray-4 border-radius-8px p-10 is-full-width is-flex is-flex-direction-column is-justify-content-space-between',
@@ -12,6 +14,10 @@ const CollectionCard = ({ name }) => {
       <p className="is-size-4 has-text-weight-semibold has-text-black">{value}</p>
     </div>
   );
+
+  // TODO: isActive toggle
+  // TODO: Favorited
+
   return (
     <div className={clsx('p-10 is-flex is-flex-grow-1', styles.card)}>
       <div className="box has-background-white is-full-width p-0 border-radius-2px is-clipped is-flex is-flex-direction-column">
@@ -20,15 +26,13 @@ const CollectionCard = ({ name }) => {
           {/* SWITCH */}
         </div>
         <div className="is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-space-between">
-          <p className={clsx('is-size-7 is-clipped is-fullwidth mr-20 p-12')}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer laoreet vehicula sodales. Donec vestibulum sodales porta. Praesent pellentesque, magna a lacinia vehicula, orci velit hendrerit urna, ut tristique ante urna sed ligula. Duis quis euismod erat, quis hendrerit dolor. Vivamus semper fringilla ipsum, et tempus mauris.
-          </p>
+          <p className={clsx('is-size-7 is-clipped is-fullwidth mr-20 p-12')}>{description}</p>
           <div className="is-flex is-justify-content-flex-start is-flex-wrap-wrap">
             <div className={clsx('my-12 is-flex pl-12', styles.stat)}>
-              {renderStats('Suggested Comments', 20)}
+              {renderStats('Suggested Comments', comments.length)}
             </div>
             <div className={clsx('my-12 is-flex pl-12', styles.stat)}>
-              {renderStats('Favorited', 20)}
+              {renderStats('Favorited', 0)}
             </div>
           </div>
         </div>
@@ -37,12 +41,9 @@ const CollectionCard = ({ name }) => {
   );
 };
 
-CollectionCard.defaultProps = {
-  name: 'Collection',
-};
-
 CollectionCard.propTypes = {
-  name: PropTypes.string,
+  isActive: PropTypes.bool.isRequired,
+  collectionData: PropTypes.object.isRequired,
 };
 
 export default CollectionCard;
