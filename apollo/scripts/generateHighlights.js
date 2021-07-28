f4683d926eddb45898a61ffd77e1a5f181ec18cd
Brendan Cody Kenny <codykenny@gmail.com>
2021-07-28 13:14:37 +0100

    Acceptable script to generate highlight terms from comment titles

--- apollo/scripts/generateHighlights.js
+++ apollo/scripts/generateHighlights.js
@@ -1,57 +1,110 @@
-sw = require('stopword');
+const sw = require('stopword');
+//console.log(sw.en);
 const data = require('../data/suggestedComments.json');
 
-console.log('starting');
+//console.log('starting');
 
 /*
-Approaches
+Approach
+ -  - for each comment, pick a highlight ngram that appears less frequently in all comments
 
- - get all ngrams, count how many times they appear in comments, select shortest, least frequently found ngrams
-
- - for each comment, pick a highlight ngram that appears less frequently in all comments
+Previous approaches that didn't show good results:
+ - get all ngrams, count how many times they appear in comments, select shortest, least frequently found ngrams buggy
 
 */
 
-const allNgramsDupes = [];
+let allNgramsDupes = [];
 
-// split each comment title into ngrams of variable length
-data.forEach((comment, i) => {
-  const commentParts = comment.title.trim().split(' ');
+function generateNgramsWithoutStopwords(comment){ // old approach
+    const highlightsForComment = [];
+    const commentParts = comment.title.replace(/[^\w\s]/gi, '').trim().split(' ');
+    // Generate ngrams excluding stop words
   for (let j = 0; j < commentParts.length; j++) {
     for (let k = 0; k < commentParts.length; k++) {
       const newNgram = commentParts.slice(j, j + k + 1);
       const commentTitleWithoutStop = sw.removeStopwords(newNgram);
       if (commentTitleWithoutStop.length === newNgram.length) {
-        allNgramsDupes.push(newNgram.join(' ').trim());
+        highlightsForComment.push(newNgram.join(' ').trim());
       }
     }
   }
+  const highlightsForCommentNoDupes = [...new Set(highlightsForComment)];
+  return highlightsForCommentNoDupes;
+}
+
+function splitByStopWord(text, stopwords){
+  // take a string, try to split by stopword
+  // if split found, recurse down into each part to see if we can split further with the rest of the stop words
+  // if we can't split the string, then we return it
+  let returnTerms = [];
+  let splitFound=false;
+  for(let i=0; i < stopwords.length && !splitFound ; i=i+1){
+    const splitText = text.split(' '+stopwords[i]+' ');
+    if(splitText.length>1){
+      splitText.forEach((term)=>{
+        returnTerms = returnTerms.concat(splitByStopWord(term, stopwords.slice(i)));
+      });
+      splitFound = true;
+    }
+  }
+  if(splitFound){
+    return returnTerms;
+  }else{
+    return [ text ];
+  }
+}
+
+// split each comment title into ngrams of variable length
+data.forEach((comment) => {
+  // const highlightsForComment = generateNgramsWithoutStopwords(comment);
+  // allNgramsDupes = allNgramsDupes.concat(highlightsForComment.slice());
+  // comment.ngrams = highlightsForComment;
+
+  // lets try splitting titles by stopwords
+  comment.ngrams = splitByStopWord(comment.title.replace(/[^\w\s]/gi, ' ').replace(/\s\s+/g, ' ').trim(), sw.en.concat(['-', '(', ')', '//']));
+
+  allNgramsDupes = allNgramsDupes.concat(comment.ngrams.slice());
 });
 const allNgrams = [...new Set(allNgramsDupes)];
-console.log(`${allNgrams.length} unique ngrams found`);
 
-/*
-data.forEach((comment) => {
-    if (comment.title.includes('your secret? Mellow jazz? Bongo drums? Huge bag of weed? -Tony Stark (Iron Man)')){
-        console.log(comment.title);
-    }
-});
-allNgrams.forEach((ngram) => {
-    if (ngram==='your secret? Mellow jazz? Bongo drums? Huge bag of weed? -Tony Stark (Iron Man)'){
-        console.log(ngram);
-    }
-}); */
-
-const ngramCounts = {};
 // count how many titles the ngram exists in
+const ngramCounts = {};
 allNgrams.forEach((anNgram) => {
   data.forEach((comment) => {
-    if (anNgram && anNgram.trim() !== '' && comment.title.includes(anNgram)) {
+    if (comment.title.includes(anNgram)) {
       ngramCounts[anNgram] = (ngramCounts[anNgram] || 0) + 1;
     }
   });
 });
 
+
+/* Pick the best highlight term for each comment
+ *  - least frequently found
+ *  - shortest
+ *  - lowest readability ( high complexity )
+ *  - least number of generic words ( programming keywords like stackoverflow list )
+ */
+const highlightTerms = [];
+data.forEach((comment)=> {
+  let foundNgram;
+  comment.ngrams.forEach((ngram)=>{
+    if (!foundNgram) {
+      foundNgram = { 'ngram':ngram, 'ngramCount': ngramCounts[ngram] };
+    }else if(ngramCounts[ngram] < foundNgram.ngramCount) {
+      foundNgram = { 'ngram':ngram, 'ngramCount': ngramCounts[ngram] };
+    }else if(ngramCounts[ngram] === foundNgram.ngramCount
+             && ngram.length < foundNgram.ngram.length){
+      foundNgram = { 'ngram':ngram, 'ngramCount': ngramCounts[ngram] };
+    }
+  });
+  highlightTerms.push(foundNgram.ngram);
+  //console.log(comment.title + '\n   '+foundNgram['ngram']+' '+foundNgram.ngramCount);
+});
+console.log(JSON.stringify([...new Set(highlightTerms)]));
+
+
+
+/*
 // get ngrams that appear in less than 5 comments
 const ngramsInfreq = [];
 for (const k in ngramCounts) {
@@ -61,12 +114,11 @@ for (const k in ngramCounts) {
 }
 console.log(`${ngramsInfreq.length} infrequently found terms (<5)`);
 
-/**/
 // find comments that have no highlighted text
 console.log(`\nMissing highlight terms for comments   >>> `);
 const commentsWithoutHighlights = [];
 data.forEach((comment) => {
-  // almost every single one
+  // we lose a few
   const ngramsInComment = [];
   ngramsInfreq.forEach((ngram) => {
     if (comment.title.includes(ngram)) {
@@ -79,16 +131,4 @@ data.forEach((comment) => {
   }
 });
 console.log(`Missing highlight terms for ${commentsWithoutHighlights.length} comments ^^^ `);
-
-
-// Find the best highlight term for each comment
-data.forEach((comment) => {
-  const highlightsForComment = [];
-  console.log(`\n${comment.title}`);
-  allNgrams.forEach((ngram) => {
-    if(comment.title.includes(ngram)){
-      highlightsForComment.push(ngram);
-      console.log(`    ${ngram}`);
-    }
-  });
-});
+*/
