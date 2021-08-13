461199b9ab5b5bca77f18d7894e8e8faad596ed5
Xandy <xandy@semasoftware.com>
2021-08-14 02:15:38 +0800

    User lodash get to set fallback

--- apollo/src/repositories/repositoryService.js
+++ apollo/src/repositories/repositoryService.js
@@ -213,9 +213,9 @@ export const aggregateRepositories = async (externalIds, includeSmartComments) =
       const repositories = repoRaw.map((repo) => {
         const { _id, externalId = '', name = '', createdAt, smartComments = [], repoStats = { userIds: [] } } = repo;
         const totalSmartComments = smartComments.length || 0;
-        const totalSmartCommenters = _.uniqBy(smartComments, (item) => item.userId.toString()).length || 0;
+        const totalSmartCommenters = _.uniqBy(smartComments, (item) => _.get(item, 'userId', 0).toString()).length || 0;
         const totalPullRequests = _.uniqBy(smartComments, 'githubMetadata.pull_number').length || 0;
-        const totalSemaUsers = repoStats.userIds.length || 0;
+        const totalSemaUsers = _.get(repoStats, 'userIds', []).length || 0;
         const data = {
           stats: {
             totalSmartComments,
