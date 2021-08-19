829881b70090cd6eb3106423bee7cfae5711804e
Abhishek <abhishek@semasoftware.com>
2021-08-19 17:46:04 +0530

    added semicolon

--- themis/src/pages/Content/modules/content-util.js
+++ themis/src/pages/Content/modules/content-util.js
@@ -673,4 +673,4 @@ export const checkSubmitButton = (semabarId, data) => {
 export const isPRPage = () => {
   const prPage = /[https://github.com/\w*/\w*/pull/\d+]/;
   return prPage.test(document.URL);
-}
+};
