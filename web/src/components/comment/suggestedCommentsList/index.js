247cb7b63ad583d3cfad5079896c8cb92409a235
Xandy Reyes <xands@outlook.com>
2021-10-08 02:00:11 +0800

    Hide add comment button when not an admin

--- web/src/components/comment/suggestedCommentsList/index.js
+++ web/src/components/comment/suggestedCommentsList/index.js
@@ -193,13 +193,15 @@ const SuggestedCommentCollection = ({ collectionId }) => {
             <div className="is-size-5 has-text-deep-black my-120 has-text-centered">
               <img src="/img/no-suggested-comments.png" className={styles['no-comments-img']} />
               <p className="is-size-7 my-25">You don't have any Custom Comments.</p>
-              <button
-                className="button is-small is-primary border-radius-4px has-text-semibold"
-                type="button"
-                onClick={goToAddPage}>
-                <FontAwesomeIcon icon={faPlus} className="mr-10" />
-                Add a Comment
-              </button>
+              { isEditable && (
+                <button
+                  className="button is-small is-primary border-radius-4px has-text-semibold"
+                  type="button"
+                  onClick={goToAddPage}>
+                  <FontAwesomeIcon icon={faPlus} className="mr-10" />
+                  Add a Comment
+                </button>
+              ) }
             </div>
           ) : (
             commentsFiltered.slice(0, NUM_PER_PAGE * page).map((item, index) => (
