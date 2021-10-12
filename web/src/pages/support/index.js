c26b99dd7c0f5b8541c02dee443c4c4e081382e1
Xandy Reyes <xands@outlook.com>
2021-10-13 00:55:54 +0800

    PHX-2005 Update Help and Support page

--- web/src/pages/support/index.js
+++ web/src/pages/support/index.js
@@ -1,14 +1,10 @@
 import React, { useState } from 'react';
 import clsx from 'clsx';
-import { chunk } from 'lodash';
 import Lottie from 'react-lottie-player';
-import animationReactions from '../../components/onboarding/animationReactions.json';
-import animationTags from '../../components/onboarding/animationTags.json';
-import animationComments from '../../components/onboarding/animationComments.json';
 import { content } from '../../components/repos/emptyRepo/content.js';
 import Helmet, { HelpSupportHelmet } from '../../components/utils/Helmet';
 import { articles } from '../../data/help';
-import styles from './help.module.scss';
+import styles from './support.module.scss';
 import withLayout from '../../components/layout';
 import { SEMA_FAQ_URL } from '../../utils/constants';
 
@@ -18,8 +14,8 @@ const HelpAndSupport = () => {
   const renderArticle = ({
     author, id, title, date, mins, link,
   }) => (
-    <a href={link} className="is-flex-grow-1 my-25" target="_blank" rel="noreferrer" key={`${id}-article`}>
-      <div className={clsx('has-text-left', styles.article)}>
+    <a href={link} className={clsx("is-flex-grow-1 my-25", styles.article)} target="_blank" rel="noreferrer" key={`${id}-article`}>
+      <div className="has-text-left p-10">
         <p className="has-text-weight-bold is-size-6 has-text-deep-black">By {author}</p>
         <p className="has-text-weight-bold is-size-5 has-text-primary">{title}</p>
         <p className="is-size-7 has-text-deep-black">{date} &#8226; {mins} minutes read</p>
@@ -30,12 +26,8 @@ const HelpAndSupport = () => {
   const renderBestPractices = () => (
     <>
       <p className="has-text-weight-semibold has-text-black is-size-2 is-size-4-mobile">Best Practices for Code Reviews</p>
-      <div className="is-full-width my-50">
-        { chunk(articles, 3).map((item) => (
-          <div className="is-flex is-flex-wrap-wrap is-justify-content-space-evenly">
-            {item.map((child) => renderArticle(child))}
-          </div>
-        )) }
+      <div className="is-flex is-align-items-center is-flex-wrap-wrap is-justify-content-space-between my-50">
+        {articles.map((child) => renderArticle(child))}
       </div>
     </>
   );
@@ -49,47 +41,43 @@ const HelpAndSupport = () => {
   }
 
   return (
-    <div className="pb-150">
+    <div className="pb-150 has-background-white">
       <Helmet {...HelpSupportHelmet} />
-      <div className="my-50 has-text-centered">
+      <div className="py-50 has-text-centered">
         <p className="has-text-weight-semibold has-text-black is-size-2 is-size-4-mobile px-20">Help and Support</p>
       </div>
       <div className={clsx(
-        'is-flex is-justify-content-space-between is-align-items-center is-flex-wrap-wrap',
+        'is-flex is-justify-content-space-between is-align-items-stretch is-flex-wrap-wrap',
         styles['animation-container'],
       )}>
         { content.map((item) => {
-          const { img, animationData, title } = item;
+          const { img, animationData, title, subtitleHtml } = item;
           return(
             <div
-              className={clsx("has-background-white p-30 mb-20", styles.lottie)}
+              key={title}
+              className={clsx("p-30 mb-20 has-background-gray-4 border-radius-14px colored-shadow", styles.lottie)}
               onMouseEnter={() => onHover(title)}
               onMouseLeave={() => onRemoveHover()}
             >
-              <p className="is-size-5 has-text-centered mb-10 has-text-weight-semibold has-text-deep-black">{title}</p>
-              {hovered === title ? (
-                <Lottie
-                  loop
-                  animationData={animationData}
-                  play={hovered === title}
-                />
-              ) : (
-                <div className="is-full-height is-flex is-align-items-center is-justify-content-center">
-                  <img src="/img/button-play.png" className={clsx(styles['button-play'], hovered === title ? 'is-invisible' : '')} />
-                  <img src={img} className={styles.img} />
-                </div>
-              )}
+              <div className="is-flex is-align-items-center" style={{ height: 300 }}>
+                {hovered === title ? (
+                  <Lottie
+                    loop
+                    animationData={animationData}
+                    play={hovered === title}
+                  />
+                ) : (
+                  <div className="is-full-height is-flex is-align-items-center is-justify-content-center">
+                    <img src="/img/button-play.png" className={clsx(styles['button-play'], hovered === title ? 'is-invisible' : '')} />
+                    <img src={img} className={styles.img} />
+                  </div>
+                )}
+              </div>
+              <p className="is-size-5 mb-10 has-text-weight-semibold has-text-primary">{title}</p>
+              <div dangerouslySetInnerHTML={{ __html: subtitleHtml }} />
             </div>
           )
         }) }
-      </div>
-      <div className="has-background-gray-9 pb-50">
-        <div className="pt-50 has-text-centered px-120 is-hidden-mobile">
-          {renderBestPractices()}
-        </div>
-        <div className="pt-50 has-text-centered px-20 is-hidden-desktop">
-          {renderBestPractices()}
-        </div>
       </div>
       <div className="has-background-white py-120 px-80">
         <div className="is-flex is-flex-wrap-wrap is-justify-content-space-evenly is-align-items-center">
@@ -103,6 +91,14 @@ const HelpAndSupport = () => {
           <div>
             <img src="/img/faq.png" alt="faq" />
           </div>
+        </div>
+      </div>
+      <div className="has-background-gray-9 pb-50">
+        <div className="pt-50 has-text-centered px-120 is-hidden-mobile">
+          {renderBestPractices()}
+        </div>
+        <div className="pt-50 has-text-centered px-20 is-hidden-desktop">
+          {renderBestPractices()}
         </div>
       </div>
     </div>
