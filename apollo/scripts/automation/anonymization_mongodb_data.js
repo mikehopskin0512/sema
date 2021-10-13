9b54c4319f29471f080cafa706504d69085b2ff6
Artsem <artem@semasoftware.com>
2021-10-13 20:05:31 +0300

    add invitations collection

--- apollo/scripts/automation/anonymization_mongodb_data.js
+++ apollo/scripts/automation/anonymization_mongodb_data.js
@@ -6,7 +6,16 @@ const non_sema_users = db.users.find({
     username: { $regex: /@(?!semasoftware\.com$)([^.]+\.)+.*$/ },
 })
 
+const non_sema_recipient = db.invitations.find({
+    recipient: { $regex: /@(?!semasoftware\.com$)([^.]+\.)+.*$/ }
+})
+
 const non_sema_users_id = new Array
+const non_sema_recipient_id = new Array
+
+while (non_sema_recipient.hasNext()) {
+    non_sema_recipient_id.push(non_sema_recipient.next()["_id"])
+}
 
 while (non_sema_users.hasNext()) {
     non_sema_users_id.push(non_sema_users.next()["_id"]);
@@ -14,6 +23,18 @@ while (non_sema_users.hasNext()) {
 
 all_collections.forEach(element => {
     const current_collection = db.getCollection(element)
-    const documents_deleted = current_collection.deleteMany({ $or: [{ userId: { $in: non_sema_users_id } }, { "_id": { $in: non_sema_users_id } }] });
+    const documents_deleted = current_collection.deleteMany({
+        $or: [{
+                userId: { $in: non_sema_users_id }
+            },
+            {
+                "_id": { $in: non_sema_users_id }
+            },
+            {
+                "_id": { $in: non_sema_recipient_id }
+            }
+
+        ]
+    });
     print(`Current collection ${element} --- deleted documents = ${documents_deleted["deletedCount"]}`)
 });
