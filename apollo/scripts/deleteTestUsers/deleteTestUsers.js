db7c3b48dfccfac96baff2647cf11dd4be5d15ad
Artsem <artem@semasoftware.com>
2021-10-14 20:37:01 +0300

    bug fix

--- apollo/scripts/deleteTestUsers/deleteTestUsers.js
+++ apollo/scripts/deleteTestUsers/deleteTestUsers.js
@@ -4,13 +4,11 @@
 // 
 // Command: mongo <DB_URL> deleteTestUsers.js 
 // 
-const DB_NAME = "phoenix-qa" // Might be used phoenix-prod 
+const DB_NAME = "phoenix_qa" // Might be used phoenix_prod 
 const USERS_NAME = ["semacodereviewtester1000@protonmail.com"]
 
-const conn = new Mongo();
-const db = conn.getDB(DB_NAME);
+var db = db.getSiblingDB(DB_NAME)
 const all_collections = db.getCollectionNames()
-
 if (!Array.isArray(USERS_NAME)) {
     print("USERS NAME should be Array")
     quit(1)
@@ -23,12 +21,12 @@ const users = db.users.find({
 const recipient = db.invitations.find({
     recipient: { $in: USERS_NAME },
 })
-
 const users_id = recipient_id = new Array
 
 while (users.hasNext()) {
-    users_id.push(users.next()["_id"])
-    print(`User to delete: ${users.next()["username"]}`)
+    user = users.next()
+    users_id.push(user["_id"])
+    print(`User to delete: ${user["username"]} --- ${user["_id"]}`)
 }
 
 while (recipient.hasNext()) {
