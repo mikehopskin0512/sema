18140cc06063d61aa15c676e1f251035d2a0753c
John Joseph Mendez <johnjosephmendez2016@gmail.com>
2021-10-20 04:07:02 +0800

    Personal Insights: removed public profile, Tag Graph: added y axis labels

--- web/src/components/LineChart/index.js
+++ web/src/components/LineChart/index.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { ResponsiveLine } from '@nivo/line';
 
 const LineChart = ({ data = [] }) => {
+  const tickValues = data?.[0]?.data?.length || 2
   return(
       <ResponsiveLine
         data={data}
-        margin={{ top: 0, right: 25, bottom: 25, left: 25 }}
+        margin={{ top: 20, right: 25, bottom: 25, left: 25 }}
         xScale={{ type: 'point' }}
         yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
         yFormat=" >-.2f"
@@ -20,7 +21,16 @@ const LineChart = ({ data = [] }) => {
           legendOffset: 0,
           legendPosition: 'middle'
         }}
-        axisLeft={null}
+        axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: '',
+            legendOffset: -40,
+            legendPosition: 'middle',
+            tickValues: tickValues
+        }}
         colors="#BFCBD3"
         lineWidth={1}
         pointSize={5}
