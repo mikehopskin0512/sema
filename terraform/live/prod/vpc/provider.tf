24afe77c800f6a2513b3276af711a57c44a5c78b
Artsem <artem@semasoftware.com>
2021-10-20 15:24:06 +0300

    test

--- terraform/live/prod/vpc/provider.tf
+++ terraform/live/prod/vpc/provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/aws"
+      version = "~> 3.38.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 3.38.0"
   region  = "us-east-1"
   profile = "sema-terraform"
 }
