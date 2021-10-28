e3eeac2d53b7a599629a05b260b04a6fca3a5d3c
John Joseph Mendez <johnjosephmendez2016@gmail.com>
2021-10-29 02:18:27 +0800

    added security

--- apollo/src/users/userModel.js
+++ apollo/src/users/userModel.js
@@ -71,7 +71,7 @@ userSchema.pre('save', async function save(next) {
   if (!this.isModified('password')) return next();
   try {
     const { username } = this.identities[0];
-    const defaultSemaCollections = ['Philosophies', 'Famous Quotes', 'My Comments', 'Common Comments', 'Security'];
+    const defaultSemaCollections = ['Philosophies', 'Famous Quotes', 'My Comments', 'Common Comments', 'Security - Mitre’s Common Weakness Enumeration (CWE)'];
     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
     
     // Creates default user collection
