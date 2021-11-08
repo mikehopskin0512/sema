3052b8e2a78e922ca8ba1011291807f180cfaeff
Artsem <artem@semasoftware.com>
2021-11-08 14:02:25 +0300

    remove ssm-parametes

--- terraform/live/stage/ssm-parameters/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region things are created in"
-  default     = "us-east-1"
-}
-variable "env" {
-  description = "Name of the environment (qa, prod etc)"
-  default     = "stage"
-}
