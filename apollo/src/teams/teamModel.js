6ee6dde2838e71393df0dd60911de488b2d024ea
Justin Ramers <jramers@semasoftware.com>
2021-11-10 07:42:13 -0800

    Make `name` required in teamModel

--- apollo/src/teams/teamModel.js
+++ apollo/src/teams/teamModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const teamSchema = new Schema({
-  name: String,
+  name: { type: String, required: true },
   avatarUrl: String,
   description: String,
   createdBy: {
