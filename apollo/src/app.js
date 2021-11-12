136499207c7cb6cd2bff140af5085a9b1580ae69
Aslan <aslanlin21@gmail.com>
2021-11-12 16:46:06 +0100

    SCR-481 increase the bodyparser limit to 10mb

--- apollo/src/app.js
+++ apollo/src/app.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(methodOverride());
 app.use(passport.initialize());
 app.disable('x-powered-by');
