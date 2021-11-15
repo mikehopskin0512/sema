fe6ac458e54c30ffd036d6d90e1622ed53862acf
Artsem <artem@semasoftware.com>
2021-11-15 14:33:08 +0300

    decomission QA1

--- terraform/live/qa1/ssm-parameters/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "sema-terraform"
-    key     = "phoenix/qa1/ssm-parameters/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "sema-terraform"
-  }
-}
