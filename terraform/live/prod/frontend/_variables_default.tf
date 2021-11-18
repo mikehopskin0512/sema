869e4618d459af642be892980314c6115087e871
Artsem <artem@semasoftware.com>
2021-11-18 18:09:49 +0300

    structure change

---
+++ terraform/live/prod/frontend/_variables_default.tf
@@ -0,0 +1,33 @@
+variable "vpc_name" {
+  type        = string
+  description = "The vpc name for other resources"
+  default     = "vpc-prod"
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "Name prefix for all resources."
+  default     = "prod"
+  validation {
+    condition     = length(compact([null, "", var.name_prefix])) > 0
+    error_message = "Name prefix must not be empty."
+  }
+}
+
+variable "ecs_task_definition_resources" {
+  type = object({
+    cpu    = number
+    memory = number
+  })
+  description = "CPU and RAM limits for the task definition."
+  default = {
+    cpu    = 1024
+    memory = 2048
+  }
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region things are created in"
+  default     = "us-east-1"
+}
