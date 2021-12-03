c6462a2f4380c957db75d22df7f433c54a36b4f7
Artem Dimitrov <a.dimitrov@andersenlab.com>
2021-12-03 13:48:25 +0000

    add sema logo url

--- themis/src/pages/Content/constants.js
+++ themis/src/pages/Content/constants.js
@@ -7,7 +7,7 @@ export const { SEMA_CLIENT_ID } = process.env;
 export const { SEMA_CLIENT_SECRET } = process.env;
 export const { AMPLITUDE_API_KEY } = process.env;
 
-export const SEMA_LOGO_URL = `${SEMA_UI_URL}`;
+export const SEMA_LOGO_URL = `${SEMA_UI_URL}/apple-touch-icon.png`;
 export const SEMA_LANDING_FAQ = `${SEMA_LANDING_URL}/faq`;
 export const SEMA_LANDING_GITHUB = `${SEMA_LANDING_URL}/gh`;
 export const SUGGESTION_URL = `${SEMA_URL}/v1/comments/suggested?q=`;
