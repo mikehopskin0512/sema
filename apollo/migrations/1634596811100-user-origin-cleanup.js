fc3a0c5120f0c584d9b6400f281a0fa8f1e5c2d6
Guillermo Mazzoni <guillermomazzoni@MacBook-Pro.local>
2021-12-08 17:25:30 -0600

    SCR-517: Enhanced script to update users who don't have a origin property

--- apollo/migrations/1634596811100-user-origin-cleanup.js
+++ apollo/migrations/1634596811100-user-origin-cleanup.js
@@ -13,7 +13,7 @@ exports.up = async (next) => {
   try {
     const users = await mongoose.connection.collection('users').find({}).toArray();
     await Promise.all(users.map(async (user) => {
-      if (user.origin === 'signup') {
+      if (user.origin === 'signup' || !user.origin) {
         const wasInvited = await mongoose.connection.collection('invitations').find({ recipient: user.username }).toArray();
         const origin = (!!wasInvited.length) ? 'invitation' : 'waitlist';
         await mongoose.connection.collection('users').findOneAndUpdate({ _id: new ObjectId(user._id) }, { $set: { origin } }, { upsert: true }); 
