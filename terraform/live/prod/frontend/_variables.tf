87603cb30f03e6a522cb75829fe74d7cc12db398
Artsem <artem@semasoftware.com>
2022-02-11 12:42:33 +0400

    fix

--- terraform/live/prod/frontend/_variables.tf
+++ terraform/live/prod/frontend/_variables.tf
@@ -1,10 +1,8 @@
 variable "phoenix_image" {
   type        = string
   description = "docker image to be used for phoenix/web; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:master-00c496bd7562870567a11df3bc032358b744deb7-production"
 }
 variable "apollo_image" {
   type        = string
   description = "docker image to be used for apollo/api; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:master-00c496bd7562870567a11df3bc032358b744deb7-production"
 }
