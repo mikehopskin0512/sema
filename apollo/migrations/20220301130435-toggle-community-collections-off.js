d402343c117747220eafce8b2454d3ab6af9919a
artdimitrov <artdimitrov@gmail.com>
2022-03-02 12:36:22 +0200

    fix

--- apollo/migrations/20220301130435-toggle-community-collections-off.js
+++ apollo/migrations/20220301130435-toggle-community-collections-off.js
@@ -1,16 +1,6 @@
-const mongoose = require('mongoose');
-const { log } = require('debug');
-
 module.exports = {
   async up(db) {
-    const mongooseUri = 'mongodb+srv://phoenix_admin:DnUKm3vsf3C3zaym@sema-cluster.tpplx.mongodb.net/phoenix_qa?authSource=admin&replicaSet=atlas-bjp57o-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-    const options = {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    };
-    await mongoose.connect(mongooseUri, options);
-
-    function getUpdateUserCollections(collections, communityCollectionsIds) {
+    function getUpdatedUserCollections(collections, communityCollectionsIds) {
       return collections.map((collection) => {
         const collectionId = collection.collectionData && collection.collectionData.toString();
         const isCommunityCollection = communityCollectionsIds.has(collectionId);
@@ -22,31 +12,31 @@ module.exports = {
     }
 
     try {
-      const communityCollections = await mongoose.connection.db
+      const communityCollections = await db
         .collection('collections')
         .find({ type: 'community' })
         .toArray();
-      const users = await mongoose.connection.db
+      const users = await db
         .collection('users')
         .find({})
         .toArray();
       const communityCollectionsIds = new Set(communityCollections.map((collection) => collection._id.toString()));
 
       await Promise.all(users.map(async (user) => {
-        const collections = getUpdateUserCollections(user.collections, communityCollectionsIds);
-        await mongoose.connection.db.collection('users').findOneAndUpdate(
-          { _id: user._id },
-          { $set: { collections } },
-        );
+        const collections = getUpdatedUserCollections(user.collections, communityCollectionsIds);
+        await db
+          .collection('users')
+          .findOneAndUpdate(
+            { _id: user._id },
+            { $set: { collections } },
+          );
       }));
     } catch (e) {
       console.log('error', e);
     }
   },
 
-  async down(db) {
-    //
+  async down() {
+    // no needed
   },
 };
-
-// main.up();
