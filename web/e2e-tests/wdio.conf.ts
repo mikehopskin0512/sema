2a53b65cf24f1146d82fa4c7313d8c015ef9783c
Damian Pagowski <damianconsu@protonmail.com>
2022-03-12 15:05:48 +0000

    limiting browser instances nbr

--- web/e2e-tests/wdio.conf.ts
+++ web/e2e-tests/wdio.conf.ts
@@ -42,7 +42,7 @@ export const config: WebdriverIO.Config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 3,
+  maxInstances: 1,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -50,7 +50,7 @@ export const config: WebdriverIO.Config = {
   //
   capabilities: [
     {
-      maxInstances: 5,
+      maxInstances: 1,
       browserName: "chrome",
       "goog:chromeOptions": {
         args: ["--disable-dev-shm-usage", "--disable-gpu", "--no-sandbox"],
