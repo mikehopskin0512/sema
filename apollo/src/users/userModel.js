af7043637101e7ad2d2668d82b8bdf8bd8f993be
mazzonig20 <guillermo@semasoftware.com>
2022-03-22 08:33:57 -0600

    WTCR-1206: Every active and new user should have 10 available invites (#1395)

--- apollo/src/users/userModel.js
+++ apollo/src/users/userModel.js
@@ -44,7 +44,7 @@ const userSchema = mongoose.Schema({
   jobTitle: String,
   company: String,
   avatarUrl: String,
-  inviteCount: { type: Number, default: 0 },
+  inviteCount: { type: Number, default: 10 },
   isActive: { type: Boolean, default: true },
   isVerified: { type: Boolean, default: false },
   isWaitlist: { type: Boolean, default: false },
