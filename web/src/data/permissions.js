d182ec208da9b3cf4a7a8dd383e71531ce686f04
Aslan <aslanlin21@gmail.com>
2022-04-07 16:47:05 +0200

    WTCR-1277 hotfix for blank page for magic link

--- web/src/data/permissions.js
+++ web/src/data/permissions.js
@@ -3,6 +3,5 @@ export const permissionsMap = {
   '/sema-admin/users/[userId]': ['canEditUsers'],
   '/sema-admin/reports': ['canViewAdmin'],
   '/sema-admin/invites': ['canViewAdmin'],
-  '/teams/invite': ['canEditUsers'],
   '/labels-management': ['canEditUsers'],
 };
