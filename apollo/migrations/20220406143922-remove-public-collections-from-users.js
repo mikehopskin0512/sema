31ed094a8c97c6f944e3925a8738ce3235418291
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2022-04-07 12:08:22 +0200

    Small fix in migration to clean collection list from users

--- apollo/migrations/20220406143922-remove-public-collections-from-users.js
+++ apollo/migrations/20220406143922-remove-public-collections-from-users.js
@@ -3,7 +3,7 @@ module.exports = {
         const communityCollections = await db.collection('collections').find({ type: 'community' }).toArray();
         const communityCollectionIdSet = new Set(communityCollections.map(collection =>  collection._id.toString()));
         const users = await db.collection('users').find({ }).toArray();
-        userIdList  = users.map(user =>  user._id)
+        
         await Promise.all(users.map(async (user) => {
           const collectionsFiltered = user.collections.filter(
             (collection) => !( collection.isActive === false && communityCollectionIdSet.has(collection.collectionData.toString()) )
