8b59bfea7f67c78a9b0c7952fe8f4dfef214de14
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2022-04-08 13:20:11 +0200

    Fix team assigned collections migration.

--- apollo/migrations/20220407143903-remove-public-collections-from-team.js
+++ apollo/migrations/20220407143903-remove-public-collections-from-team.js
@@ -5,15 +5,14 @@ module.exports = {
         const teams = await db.collection('teams').find({ }).toArray();
 
         await Promise.all(teams.map(async (team) => {
-            const collectionsFiltered = team.collections.filter(
-              (collection) => {
-                if(!collection.collectionData) {
-                  return false;
-                }
-
-                return !( collection.isActive === false && communityCollectionIdSet.has(collection.collectionData.toString()));
-              });
+          await db.collection('teams').updateOne({ _id: team._id}, {$set: { collectionsBackup: team.collections} });
+          const collectionsFiltered = team.collections.filter((collection) => collection.collectionData && !communityCollectionIdSet.has(collection.collectionData.toString()));
+          if(collectionsFiltered.length >= 1) {
             await db.collection('teams').updateOne({ _id: team._id}, {$set: { collections: collectionsFiltered} });
+            console.log('=============', team._id, ':::', collectionsFiltered.length)
+          } else {
+            console.log('up -> collectionsFiltered.length', team._id, collectionsFiltered.length)
+          }
         }));
     },
   
