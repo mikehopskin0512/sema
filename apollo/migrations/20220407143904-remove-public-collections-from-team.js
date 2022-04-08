d13d433aaf2f27099542391c70d4216fd482a6a1
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2022-04-08 13:26:00 +0200

    Add DB MIGRATION for teams more strict policy to filter out collections

--- apollo/migrations/20220407143904-remove-public-collections-from-team.js
+++ apollo/migrations/20220407143904-remove-public-collections-from-team.js
@@ -1,12 +1,12 @@
 module.exports = {
     async up(db) {
-        const communityCollections = await db.collection('collections').find({ type: 'community' }).toArray();
+        const communityCollections = await db.collection('collections').find({ type: 'team' }).toArray();
         const communityCollectionIdSet = new Set(communityCollections.map(collection =>  collection._id.toString()));
         const teams = await db.collection('teams').find({ }).toArray();
 
         await Promise.all(teams.map(async (team) => {
           await db.collection('teams').updateOne({ _id: team._id}, {$set: { collectionsBackup: team.collections} });
-          const collectionsFiltered = team.collections.filter((collection) => collection.collectionData && !communityCollectionIdSet.has(collection.collectionData.toString()));
+          const collectionsFiltered = team.collections.filter((collection) => collection.collectionData && communityCollectionIdSet.has(collection.collectionData.toString()));
           if(collectionsFiltered.length >= 1) {
             await db.collection('teams').updateOne({ _id: team._id}, {$set: { collections: collectionsFiltered} });
             console.log('=============', team._id, ':::', collectionsFiltered.length)
