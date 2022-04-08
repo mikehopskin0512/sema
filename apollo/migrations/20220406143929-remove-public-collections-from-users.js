24e49fea108b6d8047066f0d3c3c41fd2844f157
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2022-04-08 17:58:50 +0200

    Remove console.log from migrations

--- apollo/migrations/20220406143929-remove-public-collections-from-users.js
+++ apollo/migrations/20220406143929-remove-public-collections-from-users.js
@@ -7,12 +7,7 @@ module.exports = {
         await Promise.all(users.map(async (user) => {
           await db.collection('users').updateOne({ _id: user._id}, {$set: { collectionsBackup: user.collections} });
           const collectionsFiltered = user.collections.filter((collection) => collection.collectionData && !communityCollectionIdSet.has(collection.collectionData.toString()));
-          if(collectionsFiltered.length >= 1) {
-            await db.collection('users').updateOne({ _id: user._id}, {$set: { collections: collectionsFiltered} });
-            console.log('=============', user._id, ':::', collectionsFiltered.length)
-          } else {
-            console.log('up -> collectionsFiltered.length', user._id, collectionsFiltered.length)
-          }
+          await db.collection('users').updateOne({ _id: user._id}, {$set: { collections: collectionsFiltered} });
         }));
     },
   
--- apollo/migrations/20220407143904-remove-public-collections-from-team.js
+++ apollo/migrations/20220407143904-remove-public-collections-from-team.js
@@ -7,12 +7,7 @@ module.exports = {
         await Promise.all(teams.map(async (team) => {
           await db.collection('teams').updateOne({ _id: team._id}, {$set: { collectionsBackup: team.collections} });
           const collectionsFiltered = team.collections.filter((collection) => collection.collectionData && communityCollectionIdSet.has(collection.collectionData.toString()));
-          if(collectionsFiltered.length >= 1) {
-            await db.collection('teams').updateOne({ _id: team._id}, {$set: { collections: collectionsFiltered} });
-            console.log('=============', team._id, ':::', collectionsFiltered.length)
-          } else {
-            console.log('up -> collectionsFiltered.length', team._id, collectionsFiltered.length)
-          }
+          await db.collection('teams').updateOne({ _id: team._id}, {$set: { collections: collectionsFiltered} });
         }));
     },
   
