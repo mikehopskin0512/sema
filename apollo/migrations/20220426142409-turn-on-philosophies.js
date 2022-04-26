b8d8bc01c2c219218b5508b265364a46082ebcfb
Radomir Djokovic <radomir.m.djokovic@gmail.com>
2022-04-26 15:53:35 +0200

    Add migration to turn on Philosophies to existing users and teams.

---
+++ apollo/migrations/20220426142409-turn-on-philosophies.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Types: { ObjectId } } = mongoose;
+
+module.exports = {
+  async up(db) {
+    const philosophiesCollection = await db.collection('collections').findOne({ type: 'community', name: 'Philosophies2' });
+    if(!philosophiesCollection) return;
+    
+    const philosophiesCollectionId = philosophiesCollection._id;
+    await db.collection('collections').updateOne({ _id: philosophiesCollectionId }, { $set: { isAcctiveByDefault: true } });
+
+    const users = await db.collection('users').find({}).toArray();
+    await Promise.all(users.map(async (user) => {
+      const collections = user.collections.filter((collection) => !collection.collectionData.equals(philosophiesCollectionId));
+
+      await db.collection('users').updateOne({ _id: user._id }, {
+        $set: {
+          collections: [...collections, {
+            collectionData: new ObjectId(philosophiesCollectionId),
+            isActive: true,
+            _id: new ObjectId(),
+          }]
+        }
+      });
+    }));
+
+    const teams = await db.collection('teams').find({}).toArray();
+
+    await Promise.all(teams.map(async (team) => {
+      const collections = team.collections.filter((collection) => !collection.collectionData.equals(philosophiesCollectionId));
+
+      await db.collection('teams').updateOne({ _id: team._id }, {
+        $set: {
+          collections: [...collections, {
+            collectionData: new ObjectId(philosophiesCollectionId),
+            isActive: true,
+            _id: new ObjectId(),
+          }]
+        }
+      });
+    }))
+  },
+
+  async down() {
+    //
+  },
+};
