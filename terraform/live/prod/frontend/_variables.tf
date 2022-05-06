1275134e16f3ab4cb85015b7373d4f06422204dc
Artsem <artem@semasoftware.com>
2022-05-06 13:25:01 +0400

    delete default variables

--- terraform/live/prod/frontend/_variables.tf
+++ terraform/live/prod/frontend/_variables.tf
@@ -1,10 +1,8 @@
 variable "phoenix_image" {
   type        = string
   description = "docker image to be used for phoenix/web; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:master-b8ecb7cb3365c244a14faf47d1e3179da9a0880e-production"
 }
 variable "apollo_image" {
   type        = string
   description = "docker image to be used for apollo/api; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:master-b8ecb7cb3365c244a14faf47d1e3179da9a0880e-production"
 }
