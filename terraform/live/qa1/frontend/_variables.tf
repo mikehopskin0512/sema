3ffd92a7f87e876e59585a24310445beccdbffea
Artsem <artem@semasoftware.com>
2022-05-12 16:15:23 +0400

    again fix

--- terraform/live/qa1/frontend/_variables.tf
+++ terraform/live/qa1/frontend/_variables.tf
@@ -1,10 +1,15 @@
 variable "phoenix_image" {
   type        = string
   description = "docker image to be used for phoenix/web; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:qa1-2daa2e1d5a4c9ac28f86419fb00d87ff81fdf8db-production"
+  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:qa1-07124fc484c882033a750a3bdec0fb5ee182cb00-production"
 }
 variable "apollo_image" {
   type        = string
   description = "docker image to be used for apollo/api; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:qa1-2daa2e1d5a4c9ac28f86419fb00d87ff81fdf8db-production"
+  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:qa1-07124fc484c882033a750a3bdec0fb5ee182cb00-production"
+}
+variable "apollo_worker_image" {
+  type        = string
+  description = "docker image to be used for apollo/api; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:prod"
+  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:worker"
 }
