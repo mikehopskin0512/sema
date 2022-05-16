d261078dd32792bbd35339a29f9b798471324570
Artsem <artem@semasoftware.com>
2022-05-16 22:58:02 +0400

    remove deploy web

--- terraform/live/prod/frontend/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  bucket = local.s3_scr_avatars
-  policy = data.aws_iam_policy_document.s3_scr_avatars.json
-}
-
-resource "aws_s3_bucket_acl" "this" {
-  bucket = aws_s3_bucket.this.id
-  acl    = "public-read"
-}
