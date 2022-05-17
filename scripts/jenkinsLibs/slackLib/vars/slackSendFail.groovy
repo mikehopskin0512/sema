7703cdcb73119c49b517f7a661a6aa336e8206be
Artsem <artem@semasoftware.com>
2022-05-17 17:00:58 +0400

    rollback

--- scripts/jenkinsLibs/slackLib/vars/slackSendFail.groovy
+++ scripts/jenkinsLibs/slackLib/vars/slackSendFail.groovy
@@ -1,12 +1,51 @@
 #!/usr/bin/env groovy
 def call() {
-    def blocks = [
-        [
+    def blocks = [[
                     'type': 'header',
                     'text': [
                         'type': 'plain_text',
                         'text': 'Job Failed. :red_circle:',
                         'emoji': true
+                    ]
+                ],
+                [
+                    'type': 'section',
+                    'fields': [
+                        [
+                            'type': 'mrkdwn',
+                            'text': "*Job*: ${env.JOB_NAME}"
+                        ]
+                    ]
+                ],
+                [
+                    'type': 'section',
+                    'fields': [
+                        [
+                            'type': 'mrkdwn',
+                            'text': '*Project*: phoenix'
+                        ],
+                        [
+                            'type': 'mrkdwn',
+                            'text': "*Branch*: ${BRANCH_NAME}"
+                        ]
+                    ],
+                    'accessory': [
+                        'type': 'image',
+                        'image_url': 'https://www.jenkins.io/images/logos/fire/fire.png',
+                        'alt_text': 'failed'
+                    ]
+                ],
+                [
+                    'type': 'actions',
+                    'elements': [
+                        [
+                            'type': 'button',
+                            'text': [
+                                'type': 'plain_text',
+                                'text': 'View Job'
+                            ],
+                            'url': "${BUILD_URL}console"
+                        ]
                     ]
                 ]
             ]
