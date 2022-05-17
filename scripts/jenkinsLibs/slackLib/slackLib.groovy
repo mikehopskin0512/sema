72cdc53324f2bb3c83cdd93fad8a0fdf287f5636
Artsem <artem@semasoftware.com>
2022-05-17 13:37:05 +0400

    move to lib

---
+++ scripts/jenkinsLibs/slackLib/slackLib.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/env groovy
+def slackSendFail() {
+    def blocks = [
+                [
+                    'type': 'header',
+                    'text': [
+                        'type': 'plain_text',
+                        'text': 'Job Failed. :red_circle:',
+                        'emoji': true
+                ]
+                ],
+                [
+                    'type': 'section',
+                    'fields': [
+                        [
+                            'type': 'mrkdwn',
+                            'text': "*Job*: ${env.JOB_NAME}"
+                        ]
+                    ]
+                ],
+                [
+                    'type': 'section',
+                    'fields': [
+                        [
+                            'type': 'mrkdwn',
+                            'text': '*Project*: phoenix'
+                        ],
+                        [
+                            'type': 'mrkdwn',
+                            'text': "*Branch*: ${BRANCH_NAME}"
+                        ]
+                    ],
+                    'accessory': [
+                        'type': 'image',
+                        'image_url': 'https://www.jenkins.io/images/logos/fire/fire.png',
+                        'alt_text': 'failed'
+                    ]
+                ],
+                [
+                    'type': 'actions',
+                    'elements': [
+                        [
+                            'type': 'button',
+                            'text': [
+                                'type': 'plain_text',
+                                'text': 'View Job'
+                            ],
+                            'url': "${BUILD_URL}console"
+                        ]
+                    ]
+                ]
+            ]
+    slackSend(
+        blocks: blocks,
+        channel: '#phoenix-circleci-notifications',
+        color: 'danger'
+    )
+}
