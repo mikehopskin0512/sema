80d1ef96a9beaa5ed329ee4417ef8d97f36b71fc
Damian Janowski <jano@dimaion.com>
2022-05-20 18:00:12 -0300

    Lint and remove dead code.

--- apollo/src/comments/reaction/reactionService.js
+++ apollo/src/comments/reaction/reactionService.js
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import Reaction from './reactionModel';
 import logger from '../../shared/logger';
 import errors from '../../shared/errors';
@@ -15,31 +14,11 @@ export const getAllReactions = async () => {
   }
 };
 
-export const buildReactionsEmptyObject = async () => {
-  try {
-    const schema = {};
-    const reactions = await getAllReactions();
-    for (const [key, value] of Object.entries(reactions)) {
-      schema[reactions[key]._id] = 0;
-    }
-    return schema;
-  } catch (err) {
-    logger.error(err);
-    const error = new errors.NotFound(err);
-    return error;
-  }
-};
-
-export const incrementReactions = (reactionsObject = {}, reaction) => {
-  reactionsObject[reaction] += 1;
-  return reactionsObject;
-};
-
 export const getReactionIdKeys = async () => {
   try {
     const reactions = await getAllReactions();
     const reactionKeys = reactions.map((r) => r._id);
-    const keys = reactionKeys.reduce((a, b) => ((a[b] = 0), a), {});
+    const keys = reactionKeys.reduce((a, b) => ({ ...a, [b]: 0 }), {});
     return keys;
   } catch (err) {
     logger.error(err);
@@ -52,4 +31,3 @@ export const findOneByTitle = async (title) => {
   const regexp = new RegExp(`^${escapeRegExp(title)}$`, 'i');
   return await Reaction.findOne({ title: regexp, isActive: true });
 };
-
