a57e22e4b5c4fcb077528ccf4a41af03704e4840
Radomir Djokovic <radomir.djokovic@yara.com>
2022-05-31 14:36:05 +0200

    Remove merge problem

--- web/src/utils/constants.js
+++ web/src/utils/constants.js
@@ -374,8 +374,6 @@ export const KNOWLEDGE_BASE_SUMMARIES_URL =
 export const KNOWLEDGE_BASE_TAGS_URL =
   'https://intercom.help/sema-software/en/articles/6147189-how-do-i-use-tags-in-the-chrome-extension';
 
-export const KNOWLEDGE_BASE_TAGS_URL = 'https://intercom.help/sema-software/en/articles/6147189-how-do-i-use-tags-in-the-chrome-extension';
-
 export const NOTIFICATION_TYPE = {
   JOINED_SEMA: 'joined_sema'
 };
