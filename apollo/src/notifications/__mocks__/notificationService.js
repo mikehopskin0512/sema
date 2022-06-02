06ac219065e0cd0cfb84f6311a8242f3fa27ebc4
Radomir Djokovic <radomir.djokovic@yara.com>
2022-06-02 14:39:24 +0200

    Mock notificaationService for tests.

---
+++ apollo/src/notifications/__mocks__/notificationService.js
@@ -0,0 +1,11 @@
+export const getOrCreateUserAndToken = async user => {
+  return userId;
+};
+
+// ‘First_name_Last_name’ has joined Sema.
+export const addAcceptedInviteActivity = async (
+  userId,
+  user,
+  recepientId,
+  recepient
+) => ({});
