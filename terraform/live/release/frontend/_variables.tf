a782f0f7e4efc633653222b54a6bb3ed2f663679
ArtsemMaliutsin <91077796+ArtsemMaliutsin@users.noreply.github.com>
2022-06-03 15:21:55 +0400

    Update _variables.tf

--- terraform/live/release/frontend/_variables.tf
+++ terraform/live/release/frontend/_variables.tf
@@ -1,12 +1,10 @@
 variable "phoenix_image" {
   type        = string
   description = "docker image to be used for phoenix/web; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/phoenix:release-2022-06-02-e52ab4470a4419b15582d68dc4486d6f2da236c9-production"
 }
 variable "apollo_image" {
   type        = string
   description = "docker image to be used for apollo/api; eg. 091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:prod"
-  default     = "091235034633.dkr.ecr.us-east-1.amazonaws.com/apollo:release-2022-06-02-e52ab4470a4419b15582d68dc4486d6f2da236c9-production"
 }
 variable "apollo_worker_image" {
   type        = string
