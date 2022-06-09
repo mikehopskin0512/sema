db83cf867337f87b39c707049b59f6b94671a084
radomirdj <radomir.m.djokovic@gmail.com>
2022-06-09 11:58:09 +0200

    Rename historical data of teamCreate to organizationCreate migration (#1840)
    Co-authored-by: Radomir Djokovic <radomir.djokovic@yara.com>
    Co-authored-by: Artsiom Rusau <90792781+Rusau@users.noreply.github.com>

--- apollo/migrations/20220418161207-add-banners-to-user.js
+++ apollo/migrations/20220418161207-add-banners-to-user.js
@@ -12,7 +12,7 @@ module.exports = {
           {
             $addFields: {
               banners: {
-                organizationCreate: true
+                teamCreate: true
               }
             }
           }
