9cd81776f6840a65d1d1f858a9160faa314fc4a0
AkrhipovDenis <93146091+AkrhipovDenis@users.noreply.github.com>
2022-06-23 12:45:42 +0300

    East 1427 fixed skeletons (#1877)
    * EAST-1437
    
    * EAST-1427 fixed skeletons
    
    * EAST-1427 fixed reviewers adjustements
    
    * EAST-1427 fixed repo skeletons
    
    Co-authored-by: Artem <artdimitrov@gmail.com>
    Co-authored-by: Artsiom Rusau <90792781+Rusau@users.noreply.github.com>

--- web/src/components/skeletons/lineChartSkeleton.js
+++ web/src/components/skeletons/lineChartSkeleton.js
@@ -5,22 +5,20 @@ const LineChartSkeleton = (props) => (
   <ContentLoader
     speed={2}
     width="100%"
-    height={382}
-    viewBox="0 0 624 328"
+    height={462}
+    viewBox="0 0 624 408"
     backgroundColor="#d3d7d9"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="475" y="0" rx="0" ry="0" width="144" height="42" />
-    <rect x="5" y="209" rx="0" ry="0" width="43" height="119" />
-    <rect x="247" y="125" rx="0" ry="0" width="43" height="202" />
-    <rect x="327" y="215" rx="0" ry="0" width="43" height="113" />
-    <rect x="407" y="151" rx="0" ry="0" width="43" height="177" />
-    <rect x="482" y="208" rx="0" ry="0" width="43" height="120" />
-    <rect x="558" y="226" rx="0" ry="0" width="43" height="102" />
-    <rect x="162" y="285" rx="0" ry="0" width="43" height="42" />
-    <rect x="0" y="0" rx="0" ry="0" width="144" height="42" />
-    <rect x="80" y="137" rx="0" ry="0" width="43" height="192" />
+    <rect x="395" y="1" rx="0" ry="0" width="214" height="37" />
+    <rect x="60" y="179" rx="0" ry="0" width="50" height="379" />
+    <rect x="135" y="255" rx="0" ry="0" width="50" height="302" />
+    <rect x="210" y="95" rx="0" ry="0" width="50" height="462" />
+    <rect x="285" y="185" rx="0" ry="0" width="50" height="373" />
+    <rect x="360" y="121" rx="0" ry="0" width="50" height="447" />
+    <rect x="435" y="178" rx="0" ry="0" width="50" height="380" />
+    <rect x="510" y="196" rx="0" ry="0" width="50" height="362" />
   </ContentLoader>
 )
 
