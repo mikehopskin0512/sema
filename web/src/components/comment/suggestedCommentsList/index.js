56fb3f2637f6c5ae1f731d79b0480864c9305cf6
Ilya Kaminskii <ilya.kaminsky@yandex.ru>
2022-06-27 11:10:59 +0300

    hotfix of snippets search PROD

--- web/src/components/comment/suggestedCommentsList/index.js
+++ web/src/components/comment/suggestedCommentsList/index.js
@@ -91,9 +91,9 @@ const SuggestedCommentCollection = ({ collectionId }) => {
   const onSearch = ({ search, tag, language }) => {
     setIsParsing(true);
     const filtered = comments.filter((item) => {
-      const isMatchSearch = item.title.toLowerCase().includes(search.toLowerCase()) ||
-      item.comment.toLowerCase().includes(search.toLowerCase()) ||
-      item.source.name?.toLowerCase().includes(search.toLowerCase());
+      const isMatchSearch = item?.title?.toLowerCase?.().includes(search.toLowerCase()) ||
+        item?.comment?.toLowerCase?.().includes(search.toLowerCase()) ||
+        item?.source?.name?.toLowerCase?.().includes(search.toLowerCase());
       const tagIndex = findIndex(item.tags, { label: tag });
       const languageIndex = findIndex(item.tags, { label: language });
       let filterBool = true;
