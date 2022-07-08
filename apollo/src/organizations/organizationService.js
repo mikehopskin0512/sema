dc88742de62cbda8c86f25119cb7903411f3f7ba
aslanlin <58114653+aslanlin@users.noreply.github.com>
2022-07-08 12:16:42 +0200

    WEST-1434 create orgs when user login/registers (#1885)
    * WEST-1434 create orgs when user login/registers
    
    * WEST-1434 updated org meta data and fixed issues with fetching data on login
    
    * WEST-1434 updated listOrg api call to workaround the permission issue
    
    * Fixing and expanding tests together with @Aslan.
    
    * Lint.
    
    * Make test more reliable.
    
    Co-authored-by: Yana <104989982+yanakovrygina@users.noreply.github.com>
    Co-authored-by: Damian Janowski <jano@dimaion.com>
    Co-authored-by: Damian Janowski <damian@semasoftware.com>

--- apollo/src/organizations/organizationService.js
+++ apollo/src/organizations/organizationService.js
@@ -14,7 +14,10 @@ import {
 import { createUserRole } from '../userRoles/userRoleService';
 import { getRoleByName } from '../roles/roleService';
 import { semaCorporateOrganizationName } from '../config';
-import { findByUsername, getOrganizationUsersMetrics } from '../users/userService';
+import {
+  findByUsername,
+  getOrganizationUsersMetrics,
+} from '../users/userService';
 import { uploadImage } from '../utils';
 
 const {
@@ -77,6 +80,46 @@ export const getOrganizationByUrl = async (url) => {
   }
 };
 
+export const getOrganizationByGithubOrgId = async (githubOrgId) => {
+  try {
+    const organization = await Organization.findOne({
+      'orgMeta.id': githubOrgId,
+    }).exec();
+    return organization || null;
+  } catch (err) {
+    logger.error(err);
+    const error = new errors.NotFound(err);
+    return error;
+  }
+};
+
+export const createNewOrgsFromGithub = async (githubOrgs, createdBy) => {
+  try {
+    await Promise.all(
+      githubOrgs.map(async (githubOrg) => {
+        const existingOrg = await Organization.findOne({
+          'orgMeta.id': githubOrg.id,
+        });
+        if (!existingOrg) {
+          return await createOrganization({
+            name: githubOrg.login,
+            avatarUrl: githubOrg.avatar_url,
+            createdBy,
+            orgMeta: {
+              ...githubOrg,
+            },
+          });
+        }
+        return false;
+      })
+    );
+  } catch (err) {
+    logger.error(err);
+    const error = new errors.NotFound(err);
+    throw error;
+  }
+};
+
 export const getOrganizationById = async (id) => {
   try {
     const query = Organization.findOne({ _id: new ObjectId(id) });
@@ -130,7 +173,7 @@ export const createOrganization = async (data) => {
   } catch (err) {
     logger.error(err);
     const error = new errors.NotFound(err);
-    return error;
+    throw error;
   }
 };
 
@@ -175,7 +218,9 @@ export const updateOrganizationRepos = async (organizationId, repoIds) => {
 export const getOrganizationRepos = async (organizationId, params = {}) => {
   try {
     const { searchQuery = '' } = params;
-    const [organization] = await Organization.find({ _id: organizationId }).select('repos').exec();
+    const [organization] = await Organization.find({ _id: organizationId })
+      .select('repos')
+      .exec();
     const ids = organization?.repos.map((repo) => repo._id.toString()) || [];
     return getRepositories({ ids, searchQuery }, true);
   } catch (err) {
@@ -187,7 +232,9 @@ export const getOrganizationRepos = async (organizationId, params = {}) => {
 
 export async function getOrganizationMetrics(organizationId) {
   const userMetrics = await getOrganizationUsersMetrics(organizationId);
-  const commentsMetrics = await getOrganizationSmartCommentsMetrics(organizationId);
+  const commentsMetrics = await getOrganizationSmartCommentsMetrics(
+    organizationId
+  );
   let metrics = [];
   const auxDate = new Date(metricsStartDate);
   const today = new Date();
@@ -236,11 +283,17 @@ export const getOrganizationMembers = async (
       options.skip = perPage * (page - 1);
       options.limit = perPage;
     }
-    const members = await UserRole.find({ organization: organizationId }, {}, options)
+    const members = await UserRole.find(
+      { organization: organizationId },
+      {},
+      options
+    )
       .populate('user')
       .populate('role');
 
-    const totalCount = await UserRole.countDocuments({ organization: organizationId });
+    const totalCount = await UserRole.countDocuments({
+      organization: organizationId,
+    });
 
     return { members, totalCount };
   } catch (err) {
@@ -274,7 +327,11 @@ export const addOrganizationMembers = async (organization, users, role) => {
   }
 };
 
-export const updateOrganizationAvatar = async (organizationId, userId, file) => {
+export const updateOrganizationAvatar = async (
+  organizationId,
+  userId,
+  file
+) => {
   const organization = await Organization.findById(organizationId);
 
   const uploadedImage = await uploadImage(file);
