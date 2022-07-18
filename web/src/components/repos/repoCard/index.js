40e082f1d9bb91a51e970b804125d2affc7e59b9
Jonathan "Johnny" Martinez <97045862+jona-2022@users.noreply.github.com>
2022-07-18 13:37:00 -0400

    [WEST-1569] Implementation of React-Tooltip for Sync texts (#1988)
    Co-authored-by: Yana <104989982+yanakovrygina@users.noreply.github.com>

--- web/src/components/repos/repoCard/index.js
+++ web/src/components/repos/repoCard/index.js
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import Avatar from 'react-avatar';
 import { useSelector } from 'react-redux';
+import ReactTooltip from 'react-tooltip';
 import usePermission from '../../../hooks/usePermission';
 import DeleteRepoModal from "./deleteRepoModal";
 import styles from './repoCard.module.scss';
@@ -52,53 +53,56 @@ function RepoCard(props) {
   const renderAvatar = () => hasSelectedOrganization() ? selectedOrganization.organization?.avatarUrl : user.avatarUrl;
 
   return (
-  <Link href={`${PATHS.REPO}/${externalId}`}>
-    <div
-      className={clsx('p-10 is-flex is-clickable', column === 3 && styles['card-width-3c'], column === 2 && styles['card-width-2c'])}
-      aria-hidden
-      style={{zIndex: reposLength - idx}}
-    >
-      <div className={clsx('box has-background-white is-full-width p-0 border-radius-8px is-flex is-flex-direction-column', styles['card-wrapper'])}>
-        <div className={clsx("is-flex is-justify-content-space-between is-align-items-center px-16", styles[repoStatus], styles['repo-card-header'])}>
-          <div className='is-flex is-justify-content-space-between is-full-width'>
-            <div className={`${styles['tooltip-wrapper']} is-flex`}>
-              <OverflowTooltip ref={titleRef} text={name}>
-                <p ref={titleRef} className={clsx('has-text-black-900 has-text-weight-semibold is-size-5 pr-10', styles.title)}>{name}</p>
-              </OverflowTooltip>
-            </div>
-            {repoSyncTab && <RepoSyncText repoStatus={repoStatus} isCardView />}
-          </div>
-        </div>
-        <div className="is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-space-between">
-          <div className="px-12 is-flex is-justify-content-space-between is-flex-wrap-wrap">
-            {Object.keys(statLabels).map((item, i) => (
-              <div className={clsx('my-12 is-flex', styles.stat)} key={i}>
-                {renderStats(statLabels[item], repoStats?.[item])}
+  <>
+    <ReactTooltip html place="top" type="dark" effect="float"/>
+    <Link href={`${PATHS.REPO}/${externalId}`}>
+      <div
+        className={clsx('p-10 is-flex is-flex-grow-1 is-clickable', column === 3 && styles['card-width-3c'], column === 2 && styles['card-width-2c'])}
+        aria-hidden
+        style={{zIndex: reposLength - idx}}
+      >
+        <div className={clsx('box has-background-white is-full-width p-0 border-radius-8px is-flex is-flex-direction-column', styles['card-wrapper'])}>
+          <div className={clsx("is-flex is-justify-content-space-between is-align-items-center px-16", styles[repoStatus], styles['repo-card-header'])}>
+            <div className='is-flex is-justify-content-space-between is-full-width'>
+              <div className={`${styles['tooltip-wrapper']} is-flex`}>
+                <OverflowTooltip ref={titleRef} text={name}>
+                  <p ref={titleRef} className={clsx('has-text-black-900 has-text-weight-semibold is-size-5 pr-10', styles.title)}>{name}</p>
+                </OverflowTooltip>
               </div>
-            ))}
+              {repoSyncTab && <RepoSyncText repoStatus={repoStatus} isCardView />}
+            </div>
+          </div>
+          <div className="is-flex-grow-1 is-flex is-flex-direction-column is-justify-content-space-between">
+            <div className="px-12 is-flex is-justify-content-space-between is-flex-wrap-wrap">
+              {Object.keys(statLabels).map((item, i) => (
+                <div className={clsx('my-12 is-flex', styles.stat)} key={i}>
+                  {renderStats(statLabels[item], repoStats?.[item])}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className='mx-12 py-10 is-flex is-align-items-center'>
+            <Avatar
+              name={renderUserName()}
+              src={renderAvatar()}
+              size="20" 
+              textSizeRatio={2.5}
+              maxInitials={2}
+              round
+            />
+            <span className='is-size-8 ml-8 has-text-black'>{renderUserName()}</span>
           </div>
         </div>
-        <div className='mx-12 py-10 is-flex is-align-items-center'>
-          <Avatar
-            name={renderUserName()}
-            src={renderAvatar()}
-            size="20" 
-            textSizeRatio={2.5}
-            maxInitials={2}
-            round
+        {isOrganizationAdmin() && (
+          <DeleteRepoModal
+            isOpen={isDeleteRepoModalOpen}
+            onCancel={() => setDeleteRepoModalOpen(false)}
+            onConfirm={removeRepo}
           />
-          <span className='is-size-8 ml-8 has-text-black'>{renderUserName()}</span>
-        </div>
+        )}
       </div>
-      {isOrganizationAdmin() && (
-        <DeleteRepoModal
-          isOpen={isDeleteRepoModalOpen}
-          onCancel={() => setDeleteRepoModalOpen(false)}
-          onConfirm={removeRepo}
-        />
-      )}
-    </div>
-  </Link>
+    </Link>
+  </>
   );
 }
 
