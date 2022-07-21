be1660dc7d5f088f2fc2ba497ff516b12617c756
Ilya Kaminsky <49962345+Ikaminsky@users.noreply.github.com>
2022-07-21 12:09:31 +0300

    EAST-1554: rework of graph (#2014)
    * WIP: responsible social graph
    
    * responsible social graph
    
    Co-authored-by: Artem <artdimitrov@gmail.com>
    Co-authored-by: Artsiom Rusau <90792781+Rusau@users.noreply.github.com>

--- web/src/pages/[handle]/collaboration/[repoId].js
+++ web/src/pages/[handle]/collaboration/[repoId].js
@@ -1,10 +1,12 @@
 import InteractionCircleChart from '../../../components/chart/InteractionCircleChart';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { getRepoSocialGraph } from '../../../state/features/repositories/api';
 import styles from './styles.module.scss';
 import { useRouter } from 'next/router';
 import Loader from '../../../components/Loader';
 import { PATHS } from '../../../utils/constants';
+import useResizeObserver from 'use-resize-observer';
+import clsx from 'clsx';
 
 const CollaborationPublicPage = () => {
   const {
@@ -12,6 +14,7 @@ const CollaborationPublicPage = () => {
     push,
   } = useRouter();
   const [interactions, setInteractions] = useState([]);
+  const { ref: graphWrapperRef, width, height } = useResizeObserver();
   const {
     handle,
     repoId,
@@ -35,6 +38,21 @@ const CollaborationPublicPage = () => {
     await push(`${PATHS.LOGIN}?isFastForwardOnboarding=true`);
   };
 
+  const scaling = useMemo(() => {
+    const commonWidthSize = 800;
+    const commonHeightSize = 900;
+    const minimalScaleValue = 0.3;
+    const windowHeight = window.innerHeight;
+    const isFitSize = width > commonWidthSize && windowHeight > commonHeightSize;
+
+    if (isFitSize) return 1;
+    const result = ((100 * width) / commonWidthSize).toFixed(2) / 100;
+    const heightOffset = (1 - (((100 * windowHeight) / commonHeightSize).toFixed(2) / 100)) * 2;
+    const calculatedScale =  (result > 1 ? 1 : result) - (heightOffset  > 0 ? heightOffset : 0) ;
+
+    return calculatedScale > minimalScaleValue ? calculatedScale : minimalScaleValue;
+  }, [graphWrapperRef, width, height])
+
   return (
     <div className={styles['collaboration-page-wrapper']}>
       <div className={styles['collaboration-page-header']}>
@@ -44,7 +62,7 @@ const CollaborationPublicPage = () => {
         <img className={styles['collaboration-page-logo']} alt='Sema logo' src='/img/logo-full.png' />
       </div>
       <div className={styles['collaboration-page-logo-content']}>
-        <div className={styles['interactions-graph-wrapper']}>
+        <div className={styles['interactions-graph-wrapper']} ref={graphWrapperRef} style={{transform: `scale(${scaling})`}}>
           {!interactions.length ? (
             <div className='is-flex is-align-items-center is-justify-content-center' style={{ height: '50vh' }}>
               <Loader customText='Loading' />
@@ -53,8 +71,10 @@ const CollaborationPublicPage = () => {
             <InteractionCircleChart interactions={interactions} user={user} />
           )}
         </div>
-        <span className={styles['collaboration-page-title']}>Let’s create your GitHub Social Circle.</span>
-        <button className='button is-primary my-25' onClick={onStartButtonClick}>Get Started!</button>
+        <div className={styles['collaboration-control-block']}>
+          <span className={clsx(styles['collaboration-page-title'], styles['collaboration-page-title-centered'])}>Let’s create your GitHub Social Circle.</span>
+          <button className='button is-primary my-25' onClick={onStartButtonClick}>Get Started!</button>
+        </div>
       </div>
     </div>
   );
